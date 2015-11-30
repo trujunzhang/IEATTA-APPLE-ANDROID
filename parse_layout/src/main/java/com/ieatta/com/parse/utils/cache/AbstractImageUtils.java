09418abc71ab317d0d1454fbd4921801ca76c6ec
Trujun Zhang <trujunzhang@gmail.com>
2015-11-30 17:50:26 +0800

    IEATTA for Android.

--- parse_layout/src/main/java/com/ieatta/com/parse/utils/cache/AbstractImageUtils.java
+++ parse_layout/src/main/java/com/ieatta/com/parse/utils/cache/AbstractImageUtils.java
@@ -1,5 +1,22 @@
 package com.ieatta.com.parse.utils.cache;
 
+import android.graphics.Bitmap;
+import android.media.Image;
+import android.view.View;
+
+import com.ieatta.com.parse.ParseModelAbstract;
+import com.ieatta.com.parse.models.Photo;
+import com.nostra13.universalimageloader.cache.disc.impl.UnlimitedDiskCache;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.assist.FailReason;
+import com.nostra13.universalimageloader.core.listener.ImageLoadingListener;
+
+import java.io.File;
+import java.util.LinkedList;
+
+import bolts.Task;
+import bolts.TaskCompletionSource;
+
 /**
  * Created by djzhang on 11/30/15.
  */
@@ -9,125 +26,145 @@ package com.ieatta.com.parse.utils.cache;
 ///   2.1 When pushing to server, push original and thumbnail images to server.
 ///   2.2 When pushed successfully, delete offline original image.
 ///   3.  When pulling from server, just download a thumbnail image from server.
-public class AbstractImageUtils {
+public abstract class AbstractImageUtils {
 
 
-//    func getImageCache() -> SDImageCache{
-//        fatalError("Must Override")
-//    }
-//
-//    /**
-//     Query the disk cache's url path.
-//
-//     - parameter objectUUID: Photo's objectUUID
-//
-//     - returns: Image Cache's url
-//     */
-//    func getCacheImageUrl(objectUUID:String) -> NSURL{
-//        return self.getImageCache().getStoreImageUrl(objectUUID)
-//    }
-//
-//    func getCacheImageUrl(model:Photo) -> NSURL{
-//        return self.getCacheImageUrl(ParseModelAbstract.getPoint(model))
-//    }
-//
-//    func diskImageExistsWithKey(model:Photo) -> Bool{
-//        return self.getImageCache().diskImageExistsWithKey(ParseModelAbstract.getPoint(model))
-//    }
-//
-//    /**
-//     Query the disk cache synchronously after checking the memory cache.
-//
-//     - parameter objectUUID: photo's objectUUID
-//
-//     - returns: Image Cache
-//     */
-//    func getTakenPhoto(objectUUID:String) -> UIImage?{
-//        if(objectUUID.isEmpty == true){
-//            return nil
-//        }
-//
-//        return self.getImageCache().imageFromDiskCacheForKey(objectUUID)
-//    }
-//
-//    /**
-//     Query the disk cache synchronously after checking the memory cache.
-//
-//     - parameter model: Photo's instance
-//
-//     - returns: Image Cache
-//     */
-//    func getTakenPhoto(model:Photo) -> UIImage?{
-//        return self.getTakenPhoto(ParseModelAbstract.getPoint(model))
-//    }
-//
-//    func listCacheImageNames() -> NSMutableArray{
-//        return self.getImageCache().getCacheFileList()
-//    }
-//
-//    /**
-//     Save photo's image as a offline file.
-//
-//     - parameter image:           saved image
-//     - parameter model:           photo's instance
-//     - parameter completionBlock: callback variable
-//     */
-//    func saveTakenPhoto(image:UIImage,model:Photo) -> BFTask{
-//
-//        // ** Important ** Must store to Disk.
-//        self.getImageCache().storeImage(image, forKey: ParseModelAbstract.getPoint(model),toDisk: true)
-//
-//        return BFTask(result: image)
-//    }
-//
-//    /**
-//     Generate a specail type image, then save it as the offline image.
-//
-//     - parameter image: taken photo
-//     - parameter model: photo's instance
-//
-//     - returns: task's instance
-//     */
-//    func generateTakenPhoto(image:UIImage,model:Photo) -> BFTask{
-//        fatalError("Must Override")
-//    }
-//
-//    func downloadImageWithURL(URL:NSURL) -> BFTask{
-//        let downloadTask = BFTaskCompletionSource()
-//
-//        SDWebImageManager.sharedManager().downloadImageWithURL(URL, options: SDWebImageOptions.LowPriority, progress: nil) { (image, error, cacheType, finished, imageURL) -> Void in
-//            if let _image = image{
-//
-//                downloadTask.setResult(_image)
-//            }else {
-//                downloadTask.setError(error)
-//            }
-//        }
-//
-//        return downloadTask.task
-//    }
-//
-//    func downloadImageFromServer(forPhoto model:Photo,url:String) -> BFTask{
-//        /// If the image already exist on the cache folder, we don't download it from the Parse.com.
-//        if let _image = self.getTakenPhoto(model){
+    protected abstract UnlimitedDiskCache getImageCache() ;
+
+
+    /**
+     Query the disk cache's url path.
+
+     - parameter objectUUID: Photo's objectUUID
+
+     - returns: Image Cache's url
+     */
+    public File getCacheImageUrl(String objectUUID) {
+        return this.getImageCache().get(objectUUID);
+    }
+
+    public File getCacheImageUrl(Photo model) {
+        return this.getCacheImageUrl(ParseModelAbstract.getPoint(model));
+    }
+
+    public boolean diskImageExistsWithKey(Photo model) {
+        // TODO djzhang(fixiing)
+//        return this.getImageCache().diskImageExistsWithKey(ParseModelAbstract.getPoint(model));
+        return false;
+    }
+
+    /**
+     Query the disk cache synchronously after checking the memory cache.
+
+     - parameter objectUUID: photo's objectUUID
+
+     - returns: Image Cache
+     */
+    public File getTakenPhoto(String objectUUID) {
+        if(objectUUID == null || objectUUID.equals("") == true){
+            return null;
+        }
+
+//        return this.getImageCache().imageFromDiskCacheForKey(objectUUID);
+        return null;
+    }
+
+    /**
+     Query the disk cache synchronously after checking the memory cache.
+
+     - parameter model: Photo's instance
+
+     - returns: Image Cache
+     */
+    public File getTakenPhoto(Photo model) {
+        return this.getTakenPhoto(ParseModelAbstract.getPoint(model));
+    }
+
+    public LinkedList listCacheImageNames(){
+//        return this.getImageCache().getCacheFileList();
+        return null;
+    }
+
+    /**
+     Save photo's image as a offline file.
+
+     - parameter image:           saved image
+     - parameter model:           photo's instance
+     - parameter completionBlock: callback variable
+     */
+    public Task<Object> saveTakenPhoto(Bitmap image,Photo model) {
+
+        // ** Important ** Must store to Disk.
+//        this.getImageCache().storeImage(image, forKey: ParseModelAbstract.getPoint(model),toDisk: true);
+
+        TaskCompletionSource finalTask = new TaskCompletionSource();
+        finalTask.setResult(image);
+        return finalTask.getTask();
+    }
+
+    /**
+     Generate a specail type image, then save it as the offline image.
+
+     - parameter image: taken photo
+     - parameter model: photo's instance
+
+     - returns: task's instance
+     */
+    public abstract Task<Object> generateTakenPhoto(Bitmap image, Photo model) ;
+
+
+    public Task<Object> downloadImageWithURL(String URL) {
+        final TaskCompletionSource downloadTask = new TaskCompletionSource();
+
+        ImageLoader.getInstance().loadImage(URL, new ImageLoadingListener() {
+            @Override
+            public void onLoadingStarted(String imageUri, View view) {
+
+            }
+
+            @Override
+            public void onLoadingFailed(String imageUri, View view, FailReason failReason) {
+//                downloadTask.setError(failReason);
+            }
+
+            @Override
+            public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+                downloadTask.setResult(loadedImage);
+            }
+
+            @Override
+            public void onLoadingCancelled(String imageUri, View view) {
+//                downloadTask.setError(failReason);
+            }
+        });
+
+        return downloadTask.getTask();
+    }
+
+    public Task<Object> downloadImageFromServer(Photo model,String url) {
+
+        /// If the image already exist on the cache folder, we don't download it from the Parse.com.
+//        if let _image = this.getTakenPhoto(model){
 //            return BFTask(result: _image)
 //        }
 //        else if(url.isEmpty == true){
 //            return BFTask(error: NSError.getError(IEAErrorType.EmptyURL, description: "\(model.printDescription())"))
 //        }
 //        if let URL = NSURL(string: url){
-//            return self.downloadImageWithURL(URL).continueWithBlock { (task) -> AnyObject? in
+//            return this.downloadImageWithURL(URL).continueWithBlock { (task) -> AnyObject? in
 //
 //                if let _ = task.error{
 //                    return BFTask(error: NSError.getError(IEAErrorType.OnlineImage, description: "\(model.printDescription())"))
 //                }else{
-//                    return self.saveTakenPhoto(task.result as! UIImage, model: model)
+//                    return this.saveTakenPhoto(task.result as! UIImage, model: model)
 //                }
 //            }
 //        }else{
 //            return BFTask(error: NSError.getError(IEAErrorType.OnlineImage, description: "\(model.printDescription())"))
 //        }
-//    }
+
+        return null;
+    }
 
 
 }
