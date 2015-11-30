0eef28c5411280ce94f2970472e3d4068f7db5ac
Trujun Zhang <trujunzhang@gmail.com>
2015-11-30 14:32:53 +0800

    IEATTA for Android.

---
+++ app/src/main/java/com/ieatta/android/IEAApplication.java
@@ -0,0 +1,16 @@
+package com.ieatta.android;
+
+import android.app.Application;
+
+/**
+ * Created by djzhang on 11/30/15.
+ */
+public class IEAApplication extends Application{
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+
+    }
+}
--- debug/src/main/java/android/virtualbreak/com/debug/AppDebugManager.java
+++ debug/src/main/java/android/virtualbreak/com/debug/AppDebugManager.java
@@ -4,4 +4,9 @@ package android.virtualbreak.com.debug;
  * Created by djzhang on 11/30/15.
  */
 public class AppDebugManager {
+
+    public static void show() {
+        LogConfigure.setLogOption();
+        ParseLocalDatabase.listLocalDatabase();
+    }
 }
