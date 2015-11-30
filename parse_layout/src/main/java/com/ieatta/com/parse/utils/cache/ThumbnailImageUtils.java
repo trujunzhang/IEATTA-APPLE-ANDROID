245165217445994fab7595a76338095c914bfddb
Trujun Zhang <trujunzhang@gmail.com>
2015-11-30 17:58:50 +0800

    IEATTA for Android.

--- parse_layout/src/main/java/com/ieatta/com/parse/utils/cache/ThumbnailImageUtils.java
+++ parse_layout/src/main/java/com/ieatta/com/parse/utils/cache/ThumbnailImageUtils.java
@@ -1,7 +1,44 @@
 package com.ieatta.com.parse.utils.cache;
 
+import android.graphics.Bitmap;
+
+import com.ieatta.com.parse.models.Photo;
+import com.nostra13.universalimageloader.cache.disc.impl.UnlimitedDiskCache;
+
+import bolts.Task;
+
 /**
  * Created by djzhang on 11/30/15.
  */
 public class ThumbnailImageUtils extends AbstractImageUtils{
+
+    final static ThumbnailImageUtils sharedInstance  = new  ThumbnailImageUtils();
+
+    /**
+     This SDImageCache constructor's namespace is "Thumbnail". Store images on the folder: "Thumbnail/com.virtualbreak.IEATTA.thumbnail"
+
+     - returns: SDImageCache's instance
+     */
+    @Override
+    protected UnlimitedDiskCache getImageCache() {
+//        return SDImageCache.sharedImageCache();
+        return null;
+    }
+
+    /**
+     First of all, generate a thumbnail image from the original image, then store the thumbnail image as a offline cache file.
+
+     - parameter image:           original image
+     - parameter model:           Photo's instance
+     - parameter completionBlock: callback variable
+     */
+    @Override
+    public Task<Object> generateTakenPhoto(Bitmap image,Photo model) {
+//        Bitmap thumbnailImage = ImageOptimizeUtils.generateOriginalImage(image)
+
+//        return super.saveTakenPhoto(thumbnailImage, model: model)
+        return null;
+    }
+
+
 }
