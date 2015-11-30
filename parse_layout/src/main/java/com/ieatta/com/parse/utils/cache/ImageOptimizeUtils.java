83f78e26c3e8c8f7a3dc07afd52e8c62e959a258
Trujun Zhang <trujunzhang@gmail.com>
2015-11-30 18:45:38 +0800

    IEATTA for Android.

--- parse_layout/src/main/java/com/ieatta/com/parse/utils/cache/ImageOptimizeUtils.java
+++ parse_layout/src/main/java/com/ieatta/com/parse/utils/cache/ImageOptimizeUtils.java
@@ -1,7 +1,60 @@
 package com.ieatta.com.parse.utils.cache;
 
+import android.graphics.Bitmap;
+
+import com.ieatta.com.parse.models.Photo;
+import com.parse.ParseFile;
+
+import java.io.File;
+
 /**
  * Created by djzhang on 11/30/15.
  */
 public class ImageOptimizeUtils {
+
+    /**
+     Get a PFFile instance of the original image.
+
+     - parameter photo: photo's instance
+
+     - returns:  PFFile's instance of the original image
+     */
+    public static ParseFile getPFFileForOrginalImage(Photo photo) {
+        File _originalImage = OriginalImageUtils.sharedInstance.getTakenPhoto(photo);
+        if(_originalImage!=null){
+            return new ParseFile(_originalImage);
+        }
+
+        return null;
+    }
+
+    /**
+     Get a PFFile instance of the thumbnail image.
+
+     - parameter photo: photo's instance
+
+     - returns:  PFFile's instance of the thumbnail image
+     */
+    public static ParseFile getPFFileForThumbnailImage(Photo photo) {
+        File _thumbnailImage = ThumbnailImageUtils.sharedInstance.getTakenPhoto(photo);
+        if(_thumbnailImage != null){
+            return new ParseFile(_thumbnailImage);
+        }
+
+        return null;
+    }
+
+    public static Bitmap generateOriginalImage(Bitmap anImage) {
+//        let orignailImage = anImage.resizedImageWithContentMode(.ScaleAspectFit, bounds: CGSizeMake(560.0, 560.0), interpolationQuality: .Medium)
+
+//        return orignailImage;
+        return  null;
+    }
+
+    public static Bitmap generateThumbnailImage(Bitmap anImage) {
+//        let thumbnailImage = anImage.thumbnailImage(86, transparentBorder: 0, cornerRadius: 0, interpolationQuality: .Default)
+
+//        return thumbnailImage
+        return null;
+    }
 }
