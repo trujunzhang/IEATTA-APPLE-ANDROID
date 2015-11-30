d22596e1451699c98d431419f58facaf3932e893
Trujun Zhang <trujunzhang@gmail.com>
2015-11-30 15:15:02 +0800

    IEATTA for Android.

--- parse_layout/src/androidTest/java/com/ieatta/com/parse/debugspec/NewRecordSpec.java
+++ parse_layout/src/androidTest/java/com/ieatta/com/parse/debugspec/NewRecordSpec.java
@@ -1,7 +1,31 @@
 package com.ieatta.com.parse.debugspec;
 
+import android.test.InstrumentationTestCase;
+
+import com.ieatta.com.parse.async.ParseAsyncTimer;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by djzhang on 11/30/15.
  */
-public class NewRecordSpec {
+public class NewRecordSpec  extends InstrumentationTestCase{
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void testAsync() throws Exception {
+        final CountDownLatch signal = new CountDownLatch(1);
+
+        new ParseAsyncTimer().prepareTimer();
+
+//        signal.countDown();
+
+        signal.await(1000, TimeUnit.SECONDS);
+
+    }
 }
+
