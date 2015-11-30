d65a3ce61db520262a54d6822650a8ccc02d01a6
Trujun Zhang <trujunzhang@gmail.com>
2015-11-30 08:56:15 +0800

    IEATTA for Android.

---
+++ parse_layout/src/main/java/com/ieatta/com/parse/async/tasks/PushNewRecordToServerTask.java
@@ -0,0 +1,7 @@
+package com.ieatta.com.parse.async.tasks;
+
+/**
+ * Created by djzhang on 11/30/15.
+ */
+public class PushNewRecordToServerTask {
+}
---
+++ parse_layout/src/main/java/com/ieatta/com/parse/utils/cache/AbstractImageUtils.java
@@ -0,0 +1,7 @@
+package com.ieatta.com.parse.utils.cache;
+
+/**
+ * Created by djzhang on 11/30/15.
+ */
+public class AbstractImageUtils {
+}
