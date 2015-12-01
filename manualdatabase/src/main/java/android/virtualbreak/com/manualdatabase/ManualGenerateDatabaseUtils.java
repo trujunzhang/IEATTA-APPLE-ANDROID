1fd7372f116f4f856943f737438b0dfc3bdf327f
Trujun Zhang <trujunzhang@gmail.com>
2015-12-01 20:32:50 +0800

    IEATTA for Android.

--- manualdatabase/src/main/java/android/virtualbreak/com/manualdatabase/ManualGenerateDatabaseUtils.java
+++ manualdatabase/src/main/java/android/virtualbreak/com/manualdatabase/ManualGenerateDatabaseUtils.java
@@ -4,5 +4,30 @@ package android.virtualbreak.com.manualdatabase;
  * Created by djzhang on 12/1/15.
  */
 public class ManualGenerateDatabaseUtils {
+    public static void manualOfflineDatabase(){
+        if(true){
+            return; // ignour
+        }
 
+        // =============================================================================
+        //        Team
+        // =============================================================================
+        ParseManualGenerator.createTeamTable();
+
+        // =============================================================================
+        //        Restaurant
+        // =============================================================================
+        ParseManualGenerator.createRestaurantTable();
+
+        //
+        // 2. Restaurants with google map address.
+        //
+        //                ParseManualGenerator.createRestaurantTableForGoogleMapAddress(true)
+
+        //
+        // 3. Save photo for restaurant.(Test for uploading to parse.com using parse cloud.)
+        //
+        //        ParseManualGenerator.createPhotoWithPFFileForRestaurant(true)
+
+    }
 }
---
+++ manualdatabase/src/main/java/android/virtualbreak/com/manualdatabase/ParseManualGenerator.java
@@ -0,0 +1,24 @@
+package android.virtualbreak.com.manualdatabase;
+
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by djzhang on 7/18/15.
+ */
+public class ParseManualGenerator {
+    int reviewStepForEvents = 0;
+    int reviewStepForRecipes = 0;
+
+
+    public static void createTeamTable() {
+
+
+    }
+
+    public static void createRestaurantTable() {
+
+    }
+}
