3cadaaa699717ae3c617270d0ea3a0b96a164a79
Trujun Zhang <trujunzhang@gmail.com>
2015-12-02 09:07:47 +0800

    IEATTA for Android.

--- app/src/main/java/com/ieatta/android/modules/view/IEARecipeDetailViewController.java
+++ app/src/main/java/com/ieatta/android/modules/view/IEARecipeDetailViewController.java
@@ -1,7 +1,19 @@
 package com.ieatta.android.modules.view;
 
+import com.ieatta.android.modules.IEAReviewsInDetailTableViewController;
+
 /**
  * Created by djzhang on 12/1/15.
  */
-public class IEARecipeDetailViewController {
+
+enum RecipeDetailSection {
+    sectionHeader,//= 0
+    sectionOrderedPeople,//= 1
+    sectionPhotos,//= 2
+    sectionReviews,//= 3
+}
+
+public class IEARecipeDetailViewController extends IEAReviewsInDetailTableViewController {
+
+
 }
