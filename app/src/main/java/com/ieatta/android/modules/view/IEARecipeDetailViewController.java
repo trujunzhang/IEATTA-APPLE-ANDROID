32f91b60abe2ce768f3707b695145a315436397b
Trujun Zhang <trujunzhang@gmail.com>
2015-12-02 09:09:31 +0800

    IEATTA for Android.

--- app/src/main/java/com/ieatta/android/modules/view/IEARecipeDetailViewController.java
+++ app/src/main/java/com/ieatta/android/modules/view/IEARecipeDetailViewController.java
@@ -1,6 +1,8 @@
 package com.ieatta.android.modules.view;
 
 import com.ieatta.android.modules.IEAReviewsInDetailTableViewController;
+import com.ieatta.com.parse.ParseModelAbstract;
+import com.ieatta.com.parse.models.Recipe;
 
 /**
  * Created by djzhang on 12/1/15.
@@ -14,6 +16,23 @@ enum RecipeDetailSection {
 }
 
 public class IEARecipeDetailViewController extends IEAReviewsInDetailTableViewController {
+private IEARecipeDetailViewController self = this;
+
+    @Override
+    public ParseModelAbstract getPageModel(){
+        return self.orderedRecipe;
+    }
+    @Override
+    public boolean havePhotoGallery() {
+        return true;
+    }
+    @Override
+    public boolean shouldShowHUD() {
+        return true;
+    }
+
+    // Transferd Model from previous page.
+    private Recipe orderedRecipe;
 
 
 }
