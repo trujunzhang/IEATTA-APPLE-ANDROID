310a0e13102bfa940db0aea3b55df64168360d0f
Trujun Zhang <trujunzhang@gmail.com>
2015-12-02 16:05:35 +0800

    IEATTA for Android.

--- app/src/main/java/com/ieatta/android/debug/MainActivity.java
+++ app/src/main/java/com/ieatta/android/debug/MainActivity.java
@@ -1,4 +1,4 @@
-package com.ieatta.android;
+package com.ieatta.android.debug;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.ieatta.android.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,25 +30,4 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
