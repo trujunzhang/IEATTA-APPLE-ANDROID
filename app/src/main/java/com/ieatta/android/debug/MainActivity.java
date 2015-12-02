9145f9043c7c4b2e2f73d44f3f25380b1f563470
Trujun Zhang <trujunzhang@gmail.com>
2015-12-02 16:17:56 +0800

    IEATTA for Android.

--- app/src/main/java/com/ieatta/android/debug/MainActivity.java
+++ app/src/main/java/com/ieatta/android/debug/MainActivity.java
@@ -5,8 +5,14 @@ import android.os.Bundle;
 import com.ieatta.android.R;
 import com.ieatta.android.modules.IEABaseTableViewController;
 import com.ieatta.android.modules.cells.IEANearRestaurantMoreCell;
+import com.ieatta.android.modules.cells.model.IEANearRestaurantMore;
+import com.ieatta.android.modules.common.MainSegueIdentifier;
 import com.ieatta.android.modules.common.edit.IEAEditKey;
 import com.ieatta.android.modules.common.edit.SectionTitleCellModel;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 public class MainActivity extends IEABaseTableViewController {
 private MainActivity self = this;
@@ -18,6 +24,12 @@ private MainActivity self = this;
 
 
         self.setRegisterCellClassWhenSelected(IEANearRestaurantMoreCell.class, IEANearRestaurantMoreCell.layoutResId, 0);
+
+        IEANearRestaurantMore managerRestaurantItem = new IEANearRestaurantMore(R.drawable.restaurants_icon, R.string.Add_a_Restaurant, MainSegueIdentifier.editRestaurantSegueIdentifier);
+        IEANearRestaurantMore[] mores = {managerRestaurantItem};
+        List<IEANearRestaurantMore> list = Arrays.asList(mores);
+
+        self.setSectionItems(new LinkedList<Object>(list), 0);
 
 //        self.appendSectionTitleCell(new SectionTitleCellModel(IEAEditKey.Section_Title, R.string.Recipe), 1);
     }
