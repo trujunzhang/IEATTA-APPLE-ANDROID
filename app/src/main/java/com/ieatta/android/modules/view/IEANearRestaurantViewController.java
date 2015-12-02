2871ae0444de7a0c66b10c7bbbd21a4a0bd121a6
Trujun Zhang <trujunzhang@gmail.com>
2015-12-02 15:54:50 +0800

    IEATTA for Android.

--- app/src/main/java/com/ieatta/android/modules/view/IEANearRestaurantViewController.java
+++ app/src/main/java/com/ieatta/android/modules/view/IEANearRestaurantViewController.java
@@ -1,6 +1,7 @@
 package com.ieatta.android.modules.view;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 
 import com.ieatta.android.R;
 import com.ieatta.android.modules.IEASplitMasterViewController;
@@ -15,7 +16,9 @@ import com.ieatta.android.observers.LocationObserver;
 import com.ieatta.com.parse.models.Restaurant;
 import com.parse.ParseGeoPoint;
 
+import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.List;
 
 import bolts.Continuation;
 import bolts.Task;
@@ -38,7 +41,7 @@ public class IEANearRestaurantViewController extends IEASplitMasterViewControlle
         super.onCreate(savedInstanceState);
 
         // Register Cells by class.
-        self.setRegisterCellClassWhenSelected(IEANearRestaurantsCell.class,NearRestaurantSection.sectionRestaurants.ordinal(),IEANearRestaurantsCell.layoutResId);
+        self.setRegisterCellClassWhenSelected(IEANearRestaurantsCell.class, NearRestaurantSection.sectionRestaurants.ordinal(), IEANearRestaurantsCell.layoutResId);
 
         self.configModelsInMoreSection();
 
@@ -48,10 +51,15 @@ public class IEANearRestaurantViewController extends IEASplitMasterViewControlle
 
     /// Add rows for section "More".
     private void configModelsInMoreSection() {
-        self.setRegisterCellClassWhenSelected(IEANearRestaurantMoreCell.class,NearRestaurantSection.sectionMoreItems.ordinal(),IEANearRestaurantMoreCell.layoutResId);
+        self.setRegisterCellClassWhenSelected(IEANearRestaurantMoreCell.class, NearRestaurantSection.sectionMoreItems.ordinal(), IEANearRestaurantMoreCell.layoutResId);
 
-        self.appendSectionTitleCell(new SectionTitleCellModel( IEAEditKey.Section_Title, R.string.More),  NearRestaurantSection.sectionMoreItems.ordinal());
+        self.appendSectionTitleCell(new SectionTitleCellModel(IEAEditKey.Section_Title, R.string.More), NearRestaurantSection.sectionMoreItems.ordinal());
 
+        self.setSectionItems(new LinkedList<Object>(getNearRestaurantMoresItems()), NearRestaurantSection.sectionMoreItems.ordinal());
+    }
+
+    @NonNull
+    private List<IEANearRestaurantMore> getNearRestaurantMoresItems() {
         // "Manager Restaurant"
         IEANearRestaurantMore managerRestaurantItem = new IEANearRestaurantMore(R.drawable.restaurants_icon, R.string.Add_a_Restaurant, MainSegueIdentifier.editRestaurantSegueIdentifier);
 
@@ -64,8 +72,8 @@ public class IEANearRestaurantViewController extends IEASplitMasterViewControlle
         // "Read Reviews"
         IEANearRestaurantMore readReviews = new IEANearRestaurantMore(R.drawable.restaurants_icon, R.string.Read_Reviews, MainSegueIdentifier.readReviewsSegueIdentifier);
 
-        IEANearRestaurantMore[] mores = {managerRestaurantItem,searchRestaurant,managerPeople,readReviews};
-//        self.setSectionItems(new LinkedList<Object>(Arrays.asList(mores)),  NearRestaurantSection.sectionMoreItems.ordinal());
+        IEANearRestaurantMore[] mores = {managerRestaurantItem, searchRestaurant, managerPeople, readReviews};
+        return Arrays.asList(mores);
     }
 
     // MARK: Query near restaurant list.
@@ -88,7 +96,7 @@ public class IEANearRestaurantViewController extends IEASplitMasterViewControlle
                 self.fetchedRestaurants = RestaurantSortUtils.sort(self.fetchedRestaurants);
 
                 if (self.fetchedRestaurants.size() != 0) {
-                    self.appendSectionTitleCell(new SectionTitleCellModel(IEAEditKey.Section_Title,R.string.Nearby_Restaurants),  NearRestaurantSection.sectionRestaurants.ordinal());
+                    self.appendSectionTitleCell(new SectionTitleCellModel(IEAEditKey.Section_Title, R.string.Nearby_Restaurants), NearRestaurantSection.sectionRestaurants.ordinal());
                 }
                 self.setSectionItems(self.fetchedRestaurants, NearRestaurantSection.sectionRestaurants.ordinal());
 
