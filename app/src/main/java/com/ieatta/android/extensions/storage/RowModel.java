a4f539579c194136e9df9c7c59e1e7ad321b3f1c
Trujun Zhang <trujunzhang@gmail.com>
2015-12-02 13:16:10 +0800

    IEATTA for Android.

---
+++ app/src/main/java/com/ieatta/android/extensions/storage/RowModel.java
@@ -0,0 +1,10 @@
+package com.ieatta.android.extensions.storage;
+
+/**
+ * Created by djzhang on 12/2/15.
+ */
+public class RowModel {
+    public int layoutResId;
+    public Class cellClass;
+    public Object model;
+}
