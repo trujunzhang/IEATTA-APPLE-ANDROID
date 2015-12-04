b942ba18608cc023457ceb4eed6c10421154a1fd
Trujun Zhang <trujunzhang@gmail.com>
2015-12-04 18:41:40 +0800

    IEATTA for android.

--- app/src/main/java/com/ieatta/android/modules/view/edit/model/IEAEditAbstractManager.java
+++ app/src/main/java/com/ieatta/android/modules/view/edit/model/IEAEditAbstractManager.java
@@ -1,93 +1,100 @@
 package com.ieatta.android.modules.view.edit.model;
 
 import com.ieatta.android.modules.common.edit.DatePickerCellModel;
+import com.ieatta.android.modules.common.edit.EditBaseCellModel;
 import com.ieatta.android.modules.common.edit.EditCellModel;
 import com.ieatta.android.modules.common.edit.EditWaiterCellModel;
 import com.ieatta.android.modules.common.edit.IEAEditKey;
 import com.ieatta.com.parse.ParseModelAbstract;
+import com.ieatta.com.parse.models.Event;
+import com.ieatta.com.parse.models.Recipe;
+import com.ieatta.com.parse.models.Restaurant;
+import com.ieatta.com.parse.models.Team;
 
 /**
  * Created by djzhang on 12/4/15.
  */
 
 abstract class IEAEditAbstractManager implements IEAEditBaseManager {
+    private IEAEditAbstractManager self = this;
+
     @Override
     public ParseModelAbstract convertToEditModel(Object[] rowModels, ParseModelAbstract model) {
-//        for array in rowModels{
-//            let sectionModels = (array as! [AnyObject])
-//            self.convertSectionArrayToEditModel(fromSectionModels: sectionModels, to: model)
-//        }
+        for (Object array : rowModels) {
+            Object[] sectionModels = (Object[]) array;
+            self.convertSectionArrayToEditModel(sectionModels, model);
+        }
         // ***** Important ***** Update created data.
         model.updateCreatedDate();
         return model;
     }
 
     private ParseModelAbstract convertSectionArrayToEditModel(Object[] rowModels, ParseModelAbstract model) {
-//        for row in rowModels{
-//            if let editCellModel = row as? EditWaiterCellModel{
-//                updateRow(editCellModel,forModel: model)
-//            }else if let editCellModel = row as? WriteReviewInEventCellModel{
-//                updateRow(editCellModel,forModel: model)
-//            }else if let editCellModel = row as? DatePickerCellModel{
-//                updateRow(editCellModel,forModel: model)
-//            }else if let editCellModel = row as? EditCellModel{
-//                updateRow(editCellModel,forModel: model)
-//            }
-//        }
+        for (Object row : rowModels) {
+            if (row.getClass().equals(EditWaiterCellModel.class)) {
+                updateRow((EditWaiterCellModel) row, model);
+//            } else if (row.getClass().equals(WriteReviewInEventCellModel.class)) {
+//                updateRow((WriteReviewInEventCellModel) row, model);
+            } else if (row.getClass().equals(DatePickerCellModel.class)) {
+                updateRow((DatePickerCellModel) row, model);
+            } else if (row.getClass().equals(EditCellModel.class)) {
+                updateRow((EditCellModel) row, model);
+            }
+        }
         return model;
     }
 
     private void updateRow(EditCellModel row, ParseModelAbstract model) {
-//        switch(row.editKey){
-//            // ++++++++++++++++++++++ Restaurant ++++
-//            case IEAEditKey.rest_name:
-//                (model as! Restaurant).displayName = row.editValue;
-//                break;
-//            // ++++++++++++++++++++++ Event ++++
-//            case IEAEditKey.event_name:
-//                (model as! Event).displayName = row.editValue
-//                break;
-//            // ++++++++++++++++++++++ Team ++++
-//            case IEAEditKey.person_name   :
-//                (model as! Team).displayName = row.editValue
-//                break;
-//            case IEAEditKey.person_address :
-//                (model as! Team).address = row.editValue
-//                break;
-//            case IEAEditKey.person_email   :
-//                (model as! Team).email = row.editValue
-//                break;
-//            // ++++++++++++++++++++++ Recipe ++++
-//            case IEAEditKey.recipe_name :
-//                (model as! Recipe).displayName = row.editValue
-//                break;
-//            case IEAEditKey.recipe_cost :
+        switch(row.editKey){
+            // ++++++++++++++++++++++ Restaurant ++++
+            case rest_name:
+//                ((Restaurant)model).displayName = row.editValue;
+                break;
+            // ++++++++++++++++++++++ Event ++++
+            case event_name:
+//                ((Event)model).displayName = row.editValue;
+                break;
+            // ++++++++++++++++++++++ Team ++++
+            case person_name   :
+//                ((Team)model).displayName = row.editValue;
+                break;
+            case person_address :
+//                ((Team)model).address = row.editValue;
+                break;
+            case person_email   :
+//                ((Team)model).email = row.editValue;
+                break;
+            // ++++++++++++++++++++++ Recipe ++++
+            case recipe_name :
+//                ((Recipe)model).displayName = row.editValue;
+                break;
+            case recipe_cost :
 //                if let n = NSNumberFormatter().numberFromString(row.editValue) {
 //                (model as! Recipe).cost = CGFloat(n)
 //            }
-//            break;
-//            default:
-//                break;
-//        }
+            break;
+            default:
+                break;
+        }
     }
 
     private void updateRow(EditWaiterCellModel row, ParseModelAbstract model) {
-//        (model as! Event).waiter = row.editValue;
+//        ((Event)model).waiter = row.editValue;
     }
 
 //    private void updateRow(WriteReviewInEventCellModel row,ParseModelAbstract  model){
 //    }
 
     private void updateRow(DatePickerCellModel row, ParseModelAbstract model) {
-//        switch(row.editKey){
-//            case IEAEditKey.event_starttime:
-//                (model as! Event).startDate = row.date;
-//                break;
-//            case IEAEditKey.event_endtime:
-//                (model as! Event).endDate = row.date;
-//                break;
-//            default:
-//                break;
-//        }
+        switch(row.editKey){
+            case event_starttime:
+//                ((Event)model ).startDate = row.date;
+                break;
+            case event_endtime:
+//                ((Event)model  ).endDate = row.date;
+                break;
+            default:
+                break;
+        }
     }
 }
