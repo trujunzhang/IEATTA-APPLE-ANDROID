15c466bb82eae000cdef68ad72191ae9033634c8
Trujun Zhang <trujunzhang@gmail.com>
2015-12-04 19:06:46 +0800

    IEATTA for android.

--- app/src/main/java/com/ieatta/android/modules/view/edit/IEAEditRecipeViewController.java
+++ app/src/main/java/com/ieatta/android/modules/view/edit/IEAEditRecipeViewController.java
@@ -1,7 +1,44 @@
 package com.ieatta.android.modules.view.edit;
 
+import android.os.Bundle;
+
+import com.ieatta.android.R;
+import com.ieatta.android.modules.common.edit.IEAEditKey;
+import com.ieatta.android.modules.common.edit.SectionTitleCellModel;
+import com.ieatta.android.modules.view.edit.model.IEAEditBaseManager;
+import com.ieatta.android.modules.view.edit.model.IEAEditRecipeManager;
+
 /**
  * Created by djzhang on 12/1/15.
  */
+
+enum EditRecipeSection  {
+         sectionInformation ,//= 0
+         sectionPhotos      ,//= 1
+        }
 public class IEAEditRecipeViewController extends IEAEditBaseViewController{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void prepareForEditTableView() {
+        // Add rows for sections.
+        self.appendSectionTitleCell(new SectionTitleCellModel( IEAEditKey.Section_Title, R.string.Recipe_Information),  EditRecipeSection.sectionInformation.ordinal());
+    }
+
+    @Override
+    protected IEAEditBaseManager getEditManager() {
+        return new IEAEditRecipeManager();
+    }
+
+    @Override
+    protected void postSaveModelSucess() {
+//        dispatch_async(dispatch_get_main_queue(), { () -> Void in
+//                NSNotificationCenter.defaultCenter().postNotificationName(PARecipeCreatedNotification, object: nil)
+//        })
+    }
+
 }
