a9b2f2cf1955d86bad89d4cb94ab6208cfe01f1e
Trujun Zhang <trujunzhang@gmail.com>
2015-12-04 20:51:19 +0800

    IEATTA for android.

--- app/src/main/java/com/ieatta/android/modules/cells/edit/IEAEditWaiterTextFieldCell.java
+++ app/src/main/java/com/ieatta/android/modules/cells/edit/IEAEditWaiterTextFieldCell.java
@@ -1,30 +1,33 @@
 package com.ieatta.android.modules.cells.edit;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.ieatta.android.R;
 import com.ieatta.android.modules.adapter.IEAViewHolder;
+import com.ieatta.android.modules.common.edit.EditWaiterCellModel;
 import com.ieatta.com.parse.models.Restaurant;
 
 /**
  * Created by djzhang on 12/2/15.
  */
 public class IEAEditWaiterTextFieldCell  extends IEAViewHolder {
-    public static int layoutResId = R.layout.google_address_cll;
+    public static int layoutResId = R.layout.edit_waiter_text_field_cell;
 
     private IEAEditWaiterTextFieldCell self = this;
 
-    private TextView formattedAddressLabel;
+    private TextView editText;
+    private EditWaiterCellModel model;
 
     public IEAEditWaiterTextFieldCell(View itemView) {
         super(itemView);
 
-        self.formattedAddressLabel = (TextView) itemView.findViewById(R.id.formattedAddressLabel);
+        self.editText = (EditText) itemView.findViewById(R.id.editText);
     }
 
     @Override
     public void updateWithModel(Object model) {
-        Restaurant more  = (Restaurant) model;
-        self.formattedAddressLabel.setText(more.getGoogleMapAddress());
+        self.model  = (EditWaiterCellModel) model;
+        self.editText.setText(self.model.editValueResId);
     }
 }
