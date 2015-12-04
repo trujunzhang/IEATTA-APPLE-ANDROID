04eebf97afd3743319fe930ef4e025e6b4fad52d
Trujun Zhang <trujunzhang@gmail.com>
2015-12-04 19:09:17 +0800

    IEATTA for android.

--- app/src/main/java/com/ieatta/android/modules/view/edit/IEAEditRestaurantViewController.java
+++ app/src/main/java/com/ieatta/android/modules/view/edit/IEAEditRestaurantViewController.java
@@ -1,7 +1,55 @@
 package com.ieatta.android.modules.view.edit;
 
+import android.os.Bundle;
+
+import com.ieatta.android.R;
+import com.ieatta.android.modules.IEASplitMasterViewController;
+import com.ieatta.android.modules.common.edit.IEAEditKey;
+import com.ieatta.android.modules.common.edit.SectionTitleCellModel;
+import com.ieatta.android.modules.view.edit.model.IEAEditBaseManager;
+import com.ieatta.android.modules.view.edit.model.IEAEditRestaurantManager;
+
 /**
  * Created by djzhang on 12/1/15.
  */
+
+enum EditRestaurantSection  {
+         sectionInformation      ,//= 0
+         sectionPhotos           ,//= 1
+         sectionGoogleMapAddress ,//= 2
+        }
 public class IEAEditRestaurantViewController extends IEAEditBaseViewController{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void prepareForEditTableView() {
+        // Add rows for sections.
+        self.appendSectionTitleCell(new SectionTitleCellModel( IEAEditKey.Section_Title, R.string.Restaurant_Information),  EditRestaurantSection.sectionInformation.ordinal());
+
+        if(self.newModel == false){
+            self.showGoogleMapAddress(EditRestaurantSection.sectionGoogleMapAddress.ordinal());
+        }
+    }
+
+    @Override
+    protected IEAEditBaseManager getEditManager() {
+        return new IEAEditRestaurantManager();
+    }
+
+    @Override
+    protected void postSaveModelSucess() {
+//        dispatch_async(dispatch_get_main_queue(), { () -> Void in
+//                NSNotificationCenter.defaultCenter().postNotificationName(PAModelCreatedRestaurantNotification, object: nil)
+//        })
+//
+//        // If current device is iPad. we will toggle to the detail view to show it.
+//        if(self.newModel == true){
+//            IEASplitMasterViewController.showRestaurantDetailViewController(self.editedModel!)
+//        }
+    }
+
 }
