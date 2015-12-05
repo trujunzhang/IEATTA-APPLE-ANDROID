0cdb2fe83f4605f83d9fb1ca0a92a38fb4c5a0ff
Trujun Zhang <trujunzhang@gmail.com>
2015-12-05 22:18:01 +0800

    IEATTA for android.

--- debug/src/androidTest/java/com/ieatta/com/parse/debugspec/ParseModelQuerySpec.java
+++ debug/src/androidTest/java/com/ieatta/com/parse/debugspec/ParseModelQuerySpec.java
@@ -161,8 +161,106 @@ public class ParseModelQuerySpec extends InstrumentationTestCase {
 //        signal.await(10000, TimeUnit.SECONDS);
 //    }
 
+//
+//    public void testUnpinInBackgroundWithNewRecord() throws Exception {
+//        final CountDownLatch signal = new CountDownLatch(1);
+//
+//        final Team whTeam = new Team("whWithNewRecord", "whWithNewRecord@gmail.com", "whWithNewRecord.st", 123);
+//
+//        final Team countTeam = new Team();
+//        final ParseQuery teamCountQurey = countTeam.makeParseQuery();
+//        final NewRecord countNewRecord = new NewRecord();
+//        final ParseQuery newRecordCountQuery = countNewRecord.makeParseQuery();
+//
+//
+//        final int[] expectCount = {-1, 1, -1, 1};
+//        countTeam.countLocalObjects(teamCountQurey).onSuccessTask(new Continuation<Integer, Task<Integer>>() {
+//            @Override
+//            public Task<Integer> then(Task<Integer> task) throws Exception {
+//                expectCount[0] = task.getResult();
+//                return countNewRecord.countLocalObjects(newRecordCountQuery);
+//            }
+//        }).onSuccessTask(new Continuation<Integer, Task<Void>>() {
+//            @Override
+//            public Task<Void> then(Task<Integer> task) throws Exception {
+//                expectCount[2] = task.getResult();
+//                // Step02: Save it.
+//                return whTeam.pinInBackgroundWithNewRecord();
+//            }
+//        }).onSuccessTask(new Continuation<Void, Task<Void>>() {
+//            @Override
+//            public Task<Void> then(Task<Void> task) throws Exception {
+//                // Step03: List table.
+//                return ParseLocalDatabase.queryLocalDatastoreInBackground(new Team().makeParseQuery(), PQueryModelType.Team);
+//            }
+//        }).onSuccessTask(new Continuation<Void, Task<Void>>() {
+//            @Override
+//            public Task<Void> then(Task<Void> task) throws Exception {
+//                // Step03: List table.
+//                return ParseLocalDatabase.queryLocalDatastoreInBackground(new NewRecord().makeParseQuery(), PQueryModelType.NewRecord);
+//            }
+//        }).onSuccessTask(new Continuation<Void, Task<Void>>() {
+//            @Override
+//            public Task<Void> then(Task<Void> task) throws Exception {
+//                /**
+//                 * **** Important ****
+//                 * Test function here
+//                 */
+//                // Step04: Delete it.
+//                return whTeam.unpinInBackgroundWithNewRecord();
+//            }
+//        }).onSuccessTask(new Continuation<Void, Task<Void>>() {
+//            @Override
+//            public Task<Void> then(Task<Void> task) throws Exception {
+//                // Step05: List table.
+//                return ParseLocalDatabase.queryLocalDatastoreInBackground(new Team().makeParseQuery(), PQueryModelType.Team);
+//            }
+//        }).onSuccessTask(new Continuation<Void, Task<Void>>() {
+//            @Override
+//            public Task<Void> then(Task<Void> task) throws Exception {
+//                // Step03: List table.
+//                return ParseLocalDatabase.queryLocalDatastoreInBackground(new NewRecord().makeParseQuery(), PQueryModelType.NewRecord);
+//            }
+//        }).onSuccessTask(new Continuation<Void, Task<Integer>>() {
+//            @Override
+//            public Task<Integer> then(Task<Void> task) throws Exception {
+//                return countTeam.countLocalObjects(teamCountQurey);
+//            }
+//        }).onSuccessTask(new Continuation<Integer, Task<Integer>>() {
+//            @Override
+//            public Task<Integer> then(Task<Integer> task) throws Exception {
+//                expectCount[1] = task.getResult();
+//                return countNewRecord.countLocalObjects(newRecordCountQuery);
+//            }
+//        }).onSuccess(new Continuation<Integer, Void>() {
+//            @Override
+//            public Void then(Task<Integer> task) throws Exception {
+//                expectCount[3] = task.getResult();
+//                return null;
+//            }
+//        }).continueWith(new Continuation<Void, Object>() {
+//            @Override
+//            public Object then(Task<Void> task) throws Exception {
+//                if (task.isFaulted()) {
+//                    Exception error = task.getError();
+//                    String message = error.getLocalizedMessage();
+//                    String errorMessage = error.getMessage();
+//                    fail(message);
+//                    signal.countDown();
+//                    return null;
+//                }
+//                assertEquals("Save/Delete model with NewRecord ", expectCount[0], expectCount[1]);
+//                assertEquals("Save/Delete model with NewRecord ", expectCount[2], expectCount[3]);
+//                signal.countDown();
+//                return null;
+//            }
+//        });
+//
+//        signal.await(10000, TimeUnit.SECONDS);
+//    }
 
-    public void testunpinInBackgroundWithNewRecord() throws Exception {
+
+    public void testUnpinInBackgroundWithoutNewRecord() throws Exception {
         final CountDownLatch signal = new CountDownLatch(1);
 
         final Team whTeam = new Team("whWithNewRecord", "whWithNewRecord@gmail.com", "whWithNewRecord.st", 123);
@@ -184,8 +282,10 @@ public class ParseModelQuerySpec extends InstrumentationTestCase {
             @Override
             public Task<Void> then(Task<Integer> task) throws Exception {
                 expectCount[2] = task.getResult();
-                // Step02: Save it.
-                return whTeam.pinInBackgroundWithNewRecord();
+                /**
+                 * Step02: Save it.(without NewRecord)
+                 */
+                return whTeam.pinInBackgroundForModel();
             }
         }).onSuccessTask(new Continuation<Void, Task<Void>>() {
             @Override
