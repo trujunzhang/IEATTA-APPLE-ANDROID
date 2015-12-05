e840c4ba9bb4776f590f4e8825612a3001ce1f49
Trujun Zhang <trujunzhang@gmail.com>
2015-12-05 20:11:07 +0800

    IEATTA for android.

---
+++ parse_layout/src/androidTest/java/com/ieatta/com/parse/debugspec/ParseModelQuerySpec.java
@@ -0,0 +1,20 @@
+package com.ieatta.com.parse.debugspec;
+
+import android.test.InstrumentationTestCase;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by djzhang on 12/5/15.
+ */
+public class ParseModelQuerySpec extends InstrumentationTestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+
+    }
+
+    public void testUnpinInBackground() throws Exception {
+        int x = 0;
+    }
+}
