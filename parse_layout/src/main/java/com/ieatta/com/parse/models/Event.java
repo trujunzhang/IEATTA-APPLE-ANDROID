4b6a7d7f4b8be57c049181cd64d2cbfcd5479487
Trujun Zhang <trujunzhang@gmail.com>
2015-12-05 10:07:43 +0800

    IEATTA for android.

--- parse_layout/src/main/java/com/ieatta/com/parse/models/Event.java
+++ parse_layout/src/main/java/com/ieatta/com/parse/models/Event.java
@@ -69,6 +69,9 @@ private Event self = this;
 
     public Event(String displayName, Date startDate, Date endDate, String content, String remarks) {
         this();
+        self.displayName = displayName;
+        self.startDate = startDate;
+        self.endDate = endDate;
     }
 
 
