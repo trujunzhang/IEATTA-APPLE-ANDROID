77efd6fa12bb852f399a348697ac8820572cc704
Trujun Zhang <trujunzhang@gmail.com>
2015-12-06 09:00:27 +0800

    IEATTA for android.

--- parse_layout/src/main/java/com/ieatta/com/parse/ParseModelQuery.java
+++ parse_layout/src/main/java/com/ieatta/com/parse/ParseModelQuery.java
@@ -179,7 +179,6 @@ public abstract class ParseModelQuery extends ParseModelConvert {
         ParseModelQuery.findFirstLocalObjectInBackground(query).continueWith(new Continuation<ParseObject, Object>() {
             @Override
             public Object then(Task<ParseObject> task) throws Exception {
-                ParseObject object = task.getResult();
                 if (task.isFaulted()) {
                     com.parse.ParseException exception = (com.parse.ParseException) task.getError();
                     if (exception.getCode() == com.parse.ParseException.OBJECT_NOT_FOUND) {
@@ -192,7 +191,7 @@ public abstract class ParseModelQuery extends ParseModelConvert {
                         tcs.setError(task.getError());
                     }
                 } else {
-                    tcs.setResult(object);
+                    tcs.setResult(task.getResult());
                 }
                 return null;
             }
