90b363c938d64f2446baf69f474e2e3c08f2b23d
Trujun Zhang <trujunzhang@gmail.com>
2015-12-06 22:01:10 +0800

    IEATTA for android.

--- app/src/main/java/com/ieatta/android/modules/cells/IEANearRestaurantsCell.java
+++ app/src/main/java/com/ieatta/android/modules/cells/IEANearRestaurantsCell.java
@@ -5,6 +5,7 @@ import android.widget.TextView;
 
 import com.ieatta.android.R;
 import com.ieatta.android.extensions.storage.CellType;
+import com.ieatta.android.extensions.viewkit.AvatarView;
 import com.ieatta.android.modules.adapter.IEAViewHolder;
 import com.ieatta.android.modules.cells.model.IEANearRestaurantMore;
 import com.ieatta.com.parse.models.Restaurant;
@@ -17,13 +18,14 @@ public class IEANearRestaurantsCell extends IEAViewHolder {
 
     private IEANearRestaurantsCell self = this;
 
-//    @IBOutlet weak var avatarView: AvatarView!
+    private AvatarView avatarView;
 
     private TextView titleLabel;
     private TextView subtitleLabel;
 
     public IEANearRestaurantsCell(View itemView) {
         super(itemView);
+        self.avatarView = (AvatarView) itemView.findViewById(R.id.avatarView);
         self.titleLabel = (TextView) itemView.findViewById(R.id.titleTextView);
         self.subtitleLabel = (TextView) itemView.findViewById(R.id.addressTextView);
     }
@@ -34,6 +36,7 @@ public class IEANearRestaurantsCell extends IEAViewHolder {
         self.titleLabel.setText(more.displayName);
         self.subtitleLabel.setText(((Restaurant) model).getGoogleMapAddress());
 
+        self.avatarView.loadNewPhotoByModel(more,R.drawable.black_biz);
 //        self.avatarView.loadNewPhoto(byModel: model, placeHolder: UIImage.DefaultRestaurantIcon())
     }
 }
