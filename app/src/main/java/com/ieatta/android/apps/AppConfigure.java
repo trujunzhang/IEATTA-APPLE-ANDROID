dd973b7e9e3ef57fd64b54ef9cdfa1a03920555b
Trujun Zhang <trujunzhang@gmail.com>
2015-12-06 20:46:25 +0800

    IEATTA for android.

--- app/src/main/java/com/ieatta/android/apps/AppConfigure.java
+++ app/src/main/java/com/ieatta/android/apps/AppConfigure.java
@@ -2,9 +2,21 @@ package com.ieatta.android.apps;
 
 import android.content.Context;
 import android.content.res.Resources;
+import android.media.Image;
 
 import com.ieatta.android.R;
 import com.ieatta.com.parse.models.Restaurant;
+import com.nostra13.universalimageloader.cache.disc.impl.UnlimitedDiskCache;
+import com.nostra13.universalimageloader.cache.disc.naming.HashCodeFileNameGenerator;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
+import com.nostra13.universalimageloader.core.decode.BaseImageDecoder;
+import com.nostra13.universalimageloader.core.download.BaseImageDownloader;
+import com.nostra13.universalimageloader.utils.StorageUtils;
+
+import java.io.File;
 
 /**
  * Created by djzhang on 12/1/15.
@@ -13,6 +25,17 @@ public class AppConfigure {
 
     public static void setup(Context context) {
         Restaurant.Default_FormattedAddress = context.getString(R.string.Still_converting_GPS_to_Address);
+
+        File cacheDir = StorageUtils.getCacheDirectory(context);
+        ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(context)
+                .threadPriority(Thread.NORM_PRIORITY - 2) // default
+                .tasksProcessingOrder(QueueProcessingType.FIFO) // default
+                .denyCacheImageMultipleSizesInMemory()
+                .imageDownloader(new BaseImageDownloader(context)) // default
+                .imageDecoder(new BaseImageDecoder(true)) // default
+                .defaultDisplayImageOptions(DisplayImageOptions.createSimple()) // default
+                .writeDebugLogs()
+                .build();
     }
 
 }
