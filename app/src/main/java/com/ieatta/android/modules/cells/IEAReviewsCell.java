9bf1d4a6a47874b6b53e565dccf8157b83a1477b
Trujun Zhang <trujunzhang@gmail.com>
2015-12-09 13:14:55 +0800

    IEATTA for android.

--- app/src/main/java/com/ieatta/android/modules/cells/IEAReviewsCell.java
+++ app/src/main/java/com/ieatta/android/modules/cells/IEAReviewsCell.java
@@ -6,11 +6,11 @@ import android.widget.TextView;
 import com.ieatta.android.R;
 import com.ieatta.android.extensions.storage.CellType;
 import com.ieatta.android.modules.adapter.IEAViewHolder;
-import com.ieatta.android.modules.cells.model.IEANearRestaurantMore;
+import com.ieatta.com.parse.models.Review;
 
 public class IEAReviewsCell extends IEAViewHolder {
     public static CellType getType() {
-        return new CellType(IEAReviewsCell.class, R.layout.near_restaurant_cell);
+        return new CellType(IEAReviewsCell.class, R.layout.reviews_cell);
     }
 
     private IEAReviewsCell self = this;
@@ -26,7 +26,7 @@ public class IEAReviewsCell extends IEAViewHolder {
 
     @Override
     public void render(Object value) {
-        IEANearRestaurantMore more  = (IEANearRestaurantMore) value;
-        self.titleLabel.setText(more.titleResId);
+        Object object = value;
+        Review more  = (Review) value;
     }
 }
