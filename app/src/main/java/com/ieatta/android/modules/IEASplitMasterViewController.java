6991473d3a3e194c7b18a00ed866f5a05ff096f5
Trujun Zhang <trujunzhang@gmail.com>
2015-12-10 21:33:01 +0800

    IEATTA for android.

--- app/src/main/java/com/ieatta/android/modules/IEASplitMasterViewController.java
+++ app/src/main/java/com/ieatta/android/modules/IEASplitMasterViewController.java
@@ -2,6 +2,7 @@ package com.ieatta.android.modules;
 
 import com.ieatta.android.extensions.UIDevice;
 import com.ieatta.android.modules.common.MainSegueIdentifier;
+import com.ieatta.com.parse.models.Restaurant;
 
 /**
  * Created by djzhang on 12/1/15.
@@ -9,17 +10,55 @@ import com.ieatta.android.modules.common.MainSegueIdentifier;
 public class IEASplitMasterViewController extends IEABaseTableViewController{
     private IEASplitMasterViewController self = this;
 
+    // Selected model from tableview.
+    protected Restaurant selectedModel;
+
     protected void whenSelectedCellTaped(MainSegueIdentifier type){
         switch(UIDevice.currentDevice().userInterfaceIdiom){
             case Pad:
-            IEASplitMasterViewController.whenSelectedCellTapedForiPad(type,rootViewController: self.splitViewController!,selectedModel: self.selectedModel)
+            IEASplitMasterViewController.whenSelectedCellTapedForiPad(type, self, self.selectedModel);
             break;
             case Phone:
-            self.performSegueWithIdentifier(type.rawValue, sender: self)
+                IEASplitMasterViewController.whenSelectedCellTapedForPhone(type, self);
             break;
             default:
                 break;
         }
     }
+
+    static void whenSelectedCellTapedForPhone(MainSegueIdentifier type,IEAAppTableViewController sender){
+
+    }
+
+    static void whenSelectedCellTapedForiPad(MainSegueIdentifier type,IEAAppTableViewController rootViewController,Restaurant selectedModel){
+//        var viewController = UIViewController()
+//        switch(type){
+//            // 4 more cell items.
+//            case .editRestaurantSegueIdentifier:
+//            viewController = UIStoryboard.Storyboard.Controllers.editRestaurantViewController().setEditModel(Restaurant(), newModel: true)
+//            break;
+//            case .searchRestaurantSegueIdentifier:
+//            viewController =  UIStoryboard.Storyboard.Controllers.searchRestaurantViewController()
+//            break;
+//            case .managerPeopleSegueIdentifier:
+//            viewController =  UIStoryboard.Storyboard.Controllers.managerPeopleViewController()
+//            break;
+//            case .readReviewsSegueIdentifier:
+//            viewController =  UIStoryboard.Storyboard.Controllers.readReviewsViewController()
+//            break;
+//            // detail restaurant cell item.
+//            case .detailRestaurantSegueIdentifier:
+//            viewController = UIStoryboard.Storyboard.Controllers.restaurantDetailViewController().transfer(selectedModel!)
+//            break;
+//            default:
+//                break;
+//        }
+//
+//        /// Create detail viewcontroller from IEASplitDetailViewController.
+//        let detailViewController = IEASplitViewController.getDetailViewController(viewController)
+//        let splitViewController = rootViewController as! UISplitViewController
+//        splitViewController.viewControllers = [(splitViewController.viewControllers.first)!,detailViewController]
+    }
+
 
 }
