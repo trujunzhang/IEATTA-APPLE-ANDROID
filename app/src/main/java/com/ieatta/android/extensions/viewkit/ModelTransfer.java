ff4b37d42475d6ac3a52bf2eb7f46586477caac0
Trujun Zhang <trujunzhang@gmail.com>
2015-12-10 20:03:09 +0800

    IEATTA for android.

--- app/src/main/java/com/ieatta/android/extensions/viewkit/ModelTransfer.java
+++ app/src/main/java/com/ieatta/android/extensions/viewkit/ModelTransfer.java
@@ -6,5 +6,5 @@ import com.ieatta.android.extensions.storage.CellType;
  * Created by djzhang on 12/1/15.
  */
 public interface ModelTransfer {
-    public void render(Object value);
+    void render(Object value);
 }
--- app/src/main/java/com/ieatta/android/modules/adapter/IEAViewHolder.java
+++ app/src/main/java/com/ieatta/android/modules/adapter/IEAViewHolder.java
@@ -9,16 +9,23 @@ import com.ieatta.android.extensions.viewkit.ModelTransfer;
 /**
  * Created by djzhang on 12/1/15.
  */
-public abstract class IEAViewHolder extends RecyclerView.ViewHolder implements ModelTransfer,View.OnClickListener, View.OnLongClickListener {
-    private ItemClickListener clickListener;
+public abstract class IEAViewHolder extends RecyclerView.ViewHolder implements ModelTransfer, View.OnClickListener, View.OnLongClickListener {
+    private IEAViewHolder self = this;
 
+    protected boolean shouldClickItem() {
+        return true;
+    }
+
+    private ItemClickListener clickListener;
 
     public IEAViewHolder(View itemView) {
         super(itemView);
 
         itemView.setTag(itemView);
-        itemView.setOnClickListener(this);
-        itemView.setOnLongClickListener(this);
+        if(self.shouldClickItem() == true) {
+            itemView.setOnClickListener(this);
+            itemView.setOnLongClickListener(this);
+        }
     }
 
     public void setClickListener(ItemClickListener itemClickListener) {
