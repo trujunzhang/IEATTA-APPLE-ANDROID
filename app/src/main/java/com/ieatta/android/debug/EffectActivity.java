2bb707fc4eb6f7bc160c3c12a5fa6b64cd4571a2
Trujun Zhang <trujunzhang@gmail.com>
2015-12-12 07:56:21 +0800

    IEATTA for android.

---
+++ app/src/main/java/com/ieatta/android/debug/EffectActivity.java
@@ -0,0 +1,15 @@
+package com.ieatta.android.debug;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ieatta.android.R;
+
+public class EffectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_config);
+    }
+}
