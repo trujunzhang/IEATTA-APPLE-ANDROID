b178d345593bbe9c95a69b769d8c2f3cb7166a7b
Trujun Zhang <trujunzhang@gmail.com>
2015-12-11 21:19:51 +0800

    IEATTA for android.

--- manualdatabase/src/main/java/android/virtualbreak/com/manualdatabase/ActivityModelDebug.java
+++ manualdatabase/src/main/java/android/virtualbreak/com/manualdatabase/ActivityModelDebug.java
@@ -13,12 +13,14 @@ public class ActivityModelDebug {
     public static Restaurant getRestaurantForRestaurantDetail() {
         Restaurant restaurant = new Restaurant();
         restaurant.objectUUID = "1CE562A4-A978-4B75-9B7B-2F3CF9F42A04";
+        restaurant.displayName = "Francisco's Centro Vasco";
         return restaurant;
     }
 
     public static Event getEventForEventDetail() {
         Event event = new Event();
         event.objectUUID = "C9970CF0-6925-42C5-8C52-4E37A8A250A1";
+        event.displayName = "California Hot Sauce Expo";
         return event;
     }
 
@@ -33,6 +35,8 @@ public class ActivityModelDebug {
         Recipe recipe=new Recipe();
         recipe.objectUUID ="641619A3-9215-4DAA-9027-A5BCB7C65943";
 //                "B46474CD-FA02-4D7E-AE0F-B9439F4C8E8F";
+        recipe.displayName = "Amazing sticky rice!";
+        recipe.cost = 1.2f;
         return recipe;
     }
 }
