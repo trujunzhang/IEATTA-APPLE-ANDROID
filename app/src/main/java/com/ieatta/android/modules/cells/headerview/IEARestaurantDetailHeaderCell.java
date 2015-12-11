20ed6acf4fe4b38b38238ef77f70caf60e31a40d
Trujun Zhang <trujunzhang@gmail.com>
2015-12-11 21:26:54 +0800

    IEATTA for android.

--- app/src/main/java/com/ieatta/android/modules/cells/headerview/IEARestaurantDetailHeaderCell.java
+++ app/src/main/java/com/ieatta/android/modules/cells/headerview/IEARestaurantDetailHeaderCell.java
@@ -1,6 +1,7 @@
 package com.ieatta.android.modules.cells.headerview;
 
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.ieatta.android.R;
@@ -11,7 +12,7 @@ import com.ieatta.android.modules.cells.model.IEARestaurantDetailHeader;
 /**
  * Created by djzhang on 12/2/15.
  */
-public class IEARestaurantDetailHeaderCell  extends IEAViewHolder {
+public class IEARestaurantDetailHeaderCell extends IEAViewHolder {
     public static CellType getType() {
         return new CellType(IEARestaurantDetailHeaderCell.class, R.layout.restaurant_detail_header_cell);
     }
@@ -23,17 +24,39 @@ public class IEARestaurantDetailHeaderCell  extends IEAViewHolder {
 
     private IEARestaurantDetailHeaderCell self = this;
 
-    private TextView formattedAddressLabel;
+    private TextView restaurantNameLabel;
+    private ImageView ratingImageView;
+
+    private TextView divideHorizontal;
+
+    private TextView divideView1;
+    private TextView divideView2;
+
+    private TextView editButton;
+    private TextView firstButton;
+    private TextView secondButton;
+    private TextView thirdButton;
 
     public IEARestaurantDetailHeaderCell(View itemView) {
         super(itemView);
 
-//        self.formattedAddressLabel = (TextView) itemView.findViewById(R.id.formattedAddressLabel);
+        self.restaurantNameLabel = (TextView) itemView.findViewById(R.id.addressTextView);
+        self.ratingImageView = (ImageView) itemView.findViewById(R.id.addressTextView);
+
+        self.divideHorizontal = (TextView) itemView.findViewById(R.id.addressTextView);
+
+        self.divideView1 = (TextView) itemView.findViewById(R.id.addressTextView);
+        self.divideView2 = (TextView) itemView.findViewById(R.id.addressTextView);
+
+        self.editButton = (TextView) itemView.findViewById(R.id.addressTextView);
+        self.firstButton = (TextView) itemView.findViewById(R.id.addressTextView);
+        self.secondButton = (TextView) itemView.findViewById(R.id.addressTextView);
+        self.thirdButton = (TextView) itemView.findViewById(R.id.addressTextView);
     }
 
     @Override
     public void render(Object value) {
-        IEARestaurantDetailHeader more  = (IEARestaurantDetailHeader) value;
+        IEARestaurantDetailHeader more = (IEARestaurantDetailHeader) value;
 //        self.formattedAddressLabel.setText(more.model.getGoogleMapAddress());
 
     }
