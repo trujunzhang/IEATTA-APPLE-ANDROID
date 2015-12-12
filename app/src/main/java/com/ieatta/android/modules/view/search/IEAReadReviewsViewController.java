828179c6de033a527d84392967c94091a924134c
Trujun Zhang <trujunzhang@gmail.com>
2015-12-12 16:48:41 +0800

    IEATTA for android.

--- app/src/main/java/com/ieatta/android/modules/view/search/IEAReadReviewsViewController.java
+++ app/src/main/java/com/ieatta/android/modules/view/search/IEAReadReviewsViewController.java
@@ -1,7 +1,55 @@
 package com.ieatta.android.modules.view.search;
 
+import android.os.Bundle;
+
+import com.ieatta.android.cache.RatedModelReviewCount;
+import com.ieatta.android.modules.IEAReviewSegueTableViewController;
+import com.ieatta.android.modules.adapter.NSIndexPath;
+import com.ieatta.android.modules.cells.IEAReadReviewsCell;
+import com.ieatta.com.parse.ParseModelAbstract;
+import com.ieatta.com.parse.models.enums.ReviewType;
+
+enum ReadReviewsSection  {
+         sectionHeader                 ,//= 0
+         sectionRatedModelReviewCounts //= 1
+        }
 /**
  * Created by djzhang on 12/1/15.
  */
-public class IEAReadReviewsViewController {
+public class IEAReadReviewsViewController extends IEAReviewSegueTableViewController {
+private IEAReadReviewsViewController self = this;
+
+    // Selected model from tableview.
+    private RatedModelReviewCount selectedModel;
+    private int reviewType = ReviewType.Review_Restaurant.ordinal();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+//        self.setRegisterCellClass(IEAReadReviewsHeaderCell);
+//        self.setRegisterCellClassWhenSelected(IEAReadReviewsCell.getType());
+
+//        self.setSectionHeader(self.reviewType);
+    }
+
+    @Override
+    public void whenSelectedEvent(Object model, NSIndexPath indexPath) {
+        super.whenSelectedEvent(model, indexPath);
+    }
+
+    @Override
+    public ParseModelAbstract getPageModel() {
+        return super.getPageModel();
+
+    }
+
+    private void setSectionHeader(int reviewType){
+//        setSectionItems([IEAReadReviewsHeader(reviewType:reviewType,viewController: self)], forSectionIndex: ReadReviewsSection.sectionHeader.rawValue)
+    }
+
+    private void showSelectedModel(RatedModelReviewCount model){
+//        self.selectedModel = model
+//        self.prepareForSeeReviewSegueIdentifier(self.getManagerNavigationViewController())
+    }
 }
--- app/src/main/java/com/ieatta/android/modules/view/search/IEASearchRestaurantViewController.java
+++ app/src/main/java/com/ieatta/android/modules/view/search/IEASearchRestaurantViewController.java
@@ -1,7 +1,40 @@
 package com.ieatta.android.modules.view.search;
 
+import android.os.Bundle;
+
+import com.ieatta.android.modules.IEASplitDetailViewController;
+import com.ieatta.android.modules.adapter.NSIndexPath;
+import com.ieatta.android.modules.cells.IEANearRestaurantsCell;
+import com.ieatta.com.parse.ParseModelAbstract;
+import com.ieatta.com.parse.models.Restaurant;
+
+import java.util.List;
+
+enum SearchRestaurantSection  {
+         sectionRestaurants //;= 0
+        }
 /**
  * Created by djzhang on 12/1/15.
  */
-public class IEASearchRestaurantViewController {
+public class IEASearchRestaurantViewController extends IEASplitDetailViewController {
+private IEASearchRestaurantViewController self = this;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        self.setRegisterCellClassWhenSelected(IEANearRestaurantsCell.getType(),SearchRestaurantSection.sectionRestaurants.ordinal());
+    }
+
+    @Override
+    public void whenSelectedEvent(Object model, NSIndexPath indexPath) {
+        self.showSelectedModel((Restaurant) model);
+    }
+
+    private void showSelectedModel(Restaurant model){
+//        self.getManagerNavigationViewController().pushViewController(UIStoryboard.Storyboard.Controllers.restaurantDetailViewController().transfer(model), animated: true)
+    }
+
+    private void updateTableView(List<ParseModelAbstract> items ){
+        self.setSectionItems(items, SearchRestaurantSection.sectionRestaurants.ordinal());
+    }
 }
