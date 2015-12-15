be1d92dd8f1ad3538f1496eb2692d18b868c0e99
Trujun Zhang <trujunzhang@gmail.com>
2015-12-15 11:22:11 +0800

    IEATTA for android.

--- app/src/main/java/com/ieatta/android/modules/view/IEAReviewDetailViewController.java
+++ app/src/main/java/com/ieatta/android/modules/view/IEAReviewDetailViewController.java
@@ -95,18 +95,14 @@ public class IEAReviewDetailViewController extends IEABaseTableViewController {
             case Review_Event:
                 self.step = 2;
                 self.appendSectionTitleCell(new SectionTitleCellModel(IEAEditKey.Section_Title, R.string.Event_Information), 2);
-//
                 self.setRegisterCellClass(IEARestaurantEventsCell.getType(), 2);
                 self.setSectionItems(CollectionUtils.createList(model), 2);
-//
                 self.showReviewForModelCells(((Event) model).belongToModel);
                 break;
             default:
                 // Add Review Content cell.
                 int rowCount = self.step;
                 self.setRegisterCellClass(IEAReviewDetailCell.getType(), rowCount);
-                /// 1. Set current review index.
-//                self.getTableViewHeightInfo().setReviewSectionIndex(rowCount);
                 // 2. Add section title cell.
                 self.appendSectionTitleCell(new SectionTitleCellModel(IEAEditKey.Section_Title, R.string.Review_Highlights), rowCount);
                 /// 3. Set section items.
