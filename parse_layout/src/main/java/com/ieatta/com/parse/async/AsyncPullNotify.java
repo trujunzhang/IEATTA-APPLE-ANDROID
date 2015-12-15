c14c69a061b4e3e23247ab1b5b858154aadcce09
Trujun Zhang <trujunzhang@gmail.com>
2015-12-15 20:10:46 +0800

    IEATTA for android.

--- parse_layout/src/main/java/com/ieatta/com/parse/async/AsyncPullNotify.java
+++ parse_layout/src/main/java/com/ieatta/com/parse/async/AsyncPullNotify.java
@@ -1,7 +1,23 @@
 package com.ieatta.com.parse.async;
 
+import com.ieatta.com.parse.ParseModelAbstract;
+import com.ieatta.com.parse.models.enums.PQueryModelType;
+
 /**
  * Created by djzhang on 11/30/15.
  */
 public class AsyncPullNotify {
+
+    public static void notify(ParseModelAbstract model) {
+        switch (model.getModelType()) {
+            case Restaurant:
+//                        NSNotificationCenter.defaultCenter().postNotificationName(.PAModelCreatedRestaurantNotification, null);
+                break;
+            case Review:
+                break;
+            default:
+                break;
+        }
+
+    }
 }
