07df215513a9b86bb16dc45ed885b986e098b04f
Trujun Zhang <trujunzhang@gmail.com>
2015-12-19 22:32:35 +0800

    IEATTA for android.

--- app/src/main/java/com/ieatta/android/IEAApplication.java
+++ app/src/main/java/com/ieatta/android/IEAApplication.java
@@ -6,6 +6,7 @@ import android.virtualbreak.com.debug.AppDebugManager;
 import com.ieatta.android.apps.AppConfigure;
 import com.ieatta.com.parse.ParseAPI;
 import com.ieatta.com.parse.async.ParseAsyncTimer;
+import com.ieatta.com.parse.utils.cache.CacheImageUtils;
 
 /**
  * Created by djzhang on 11/30/15.
@@ -19,8 +20,19 @@ public class IEAApplication extends Application{
         AppConfigure.setup(this);
         ParseAPI.setup(this);
 
+
+        /// Clean Cache folder contains downloaded original images.
+        CacheImageUtils.sharedInstance.clearCacheDisk();
+
+        // Async database in background automately
+        // TODO: djzhang(used)
+//        new ParseAsyncTimer().prepareTimer();
+
+        /**
+         Show all tables data on the offline database,when have some issues.
+
+         */
         AppDebugManager.show();
 
-//        new ParseAsyncTimer().prepareTimer();
     }
 }
