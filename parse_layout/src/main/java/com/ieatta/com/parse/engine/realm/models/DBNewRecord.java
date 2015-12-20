191f4c2238f3d1a659f187f5a039a4c1d2603092
Trujun Zhang <trujunzhang@gmail.com>
2015-12-20 21:42:59 +0800

    IEATTA for android.

--- parse_layout/src/main/java/com/ieatta/com/parse/engine/realm/models/DBNewRecord.java
+++ parse_layout/src/main/java/com/ieatta/com/parse/engine/realm/models/DBNewRecord.java
@@ -1,5 +1,9 @@
 package com.ieatta.com.parse.engine.realm.models;
 
+import com.ieatta.com.parse.models.enums.PQueryModelType;
+
+import java.util.Date;
+
 import io.realm.RealmObject;
 
 /**
@@ -7,5 +11,50 @@ import io.realm.RealmObject;
  */
 public class DBNewRecord extends RealmObject {
 
+    public String UUID = "";
+    public Date objectCreatedDate = new Date();
 
+    public int modelType = PQueryModelType.unkown.ordinal();
+    public String modelPoint = "";
+    public Date modelCreatedDate;
+
+    public String getUUID() {
+        return UUID;
+    }
+
+    public void setUUID(String UUID) {
+        this.UUID = UUID;
+    }
+
+    public Date getObjectCreatedDate() {
+        return objectCreatedDate;
+    }
+
+    public void setObjectCreatedDate(Date objectCreatedDate) {
+        this.objectCreatedDate = objectCreatedDate;
+    }
+
+    public int getModelType() {
+        return modelType;
+    }
+
+    public void setModelType(int modelType) {
+        this.modelType = modelType;
+    }
+
+    public String getModelPoint() {
+        return modelPoint;
+    }
+
+    public void setModelPoint(String modelPoint) {
+        this.modelPoint = modelPoint;
+    }
+
+    public Date getModelCreatedDate() {
+        return modelCreatedDate;
+    }
+
+    public void setModelCreatedDate(Date modelCreatedDate) {
+        this.modelCreatedDate = modelCreatedDate;
+    }
 }
--- parse_layout/src/main/java/com/ieatta/com/parse/engine/realm/models/DBPeopleInEvent.java
+++ parse_layout/src/main/java/com/ieatta/com/parse/engine/realm/models/DBPeopleInEvent.java
@@ -1,5 +1,7 @@
 package com.ieatta.com.parse.engine.realm.models;
 
+import java.util.Date;
+
 import io.realm.RealmObject;
 
 /**
@@ -8,4 +10,41 @@ import io.realm.RealmObject;
 public class DBPeopleInEvent extends RealmObject {
 
 
+    public String UUID = "";
+    public Date objectCreatedDate = new Date();
+
+    public String userRef = "";
+    public String eventRef = "";
+
+    public String getUUID() {
+        return UUID;
+    }
+
+    public void setUUID(String UUID) {
+        this.UUID = UUID;
+    }
+
+    public Date getObjectCreatedDate() {
+        return objectCreatedDate;
+    }
+
+    public void setObjectCreatedDate(Date objectCreatedDate) {
+        this.objectCreatedDate = objectCreatedDate;
+    }
+
+    public String getUserRef() {
+        return userRef;
+    }
+
+    public void setUserRef(String userRef) {
+        this.userRef = userRef;
+    }
+
+    public String getEventRef() {
+        return eventRef;
+    }
+
+    public void setEventRef(String eventRef) {
+        this.eventRef = eventRef;
+    }
 }
