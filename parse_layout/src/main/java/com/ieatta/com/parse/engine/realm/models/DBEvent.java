05b1dc349cab268e8d0ebffe1df0aa5021c4f1de
Trujun Zhang <trujunzhang@gmail.com>
2015-12-20 21:38:59 +0800

    IEATTA for android.

--- parse_layout/src/main/java/com/ieatta/com/parse/engine/realm/models/DBEvent.java
+++ parse_layout/src/main/java/com/ieatta/com/parse/engine/realm/models/DBEvent.java
@@ -1,5 +1,9 @@
 package com.ieatta.com.parse.engine.realm.models;
 
+import com.ieatta.com.parse.models.enums.ParseModelFlag;
+
+import java.util.Date;
+
 import io.realm.RealmObject;
 
 /**
@@ -7,5 +11,96 @@ import io.realm.RealmObject;
  */
 public class DBEvent extends RealmObject {
 
+    public String UUID = "";
+    public Date objectCreatedDate = new Date();
+    public String displayName = "";
 
+    public Date startDate = new Date();
+    public Date endDate;
+    public String whatToEat = "";
+    public String remarks = "";
+    public String waiter = "";
+    public String restaurantRef = "";
+
+    public int flag = ParseModelFlag.Normal.ordinal();
+
+    public String getUUID() {
+        return UUID;
+    }
+
+    public void setUUID(String UUID) {
+        this.UUID = UUID;
+    }
+
+    public Date getObjectCreatedDate() {
+        return objectCreatedDate;
+    }
+
+    public void setObjectCreatedDate(Date objectCreatedDate) {
+        this.objectCreatedDate = objectCreatedDate;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getWhatToEat() {
+        return whatToEat;
+    }
+
+    public void setWhatToEat(String whatToEat) {
+        this.whatToEat = whatToEat;
+    }
+
+    public String getRemarks() {
+        return remarks;
+    }
+
+    public void setRemarks(String remarks) {
+        this.remarks = remarks;
+    }
+
+    public String getWaiter() {
+        return waiter;
+    }
+
+    public void setWaiter(String waiter) {
+        this.waiter = waiter;
+    }
+
+    public String getRestaurantRef() {
+        return restaurantRef;
+    }
+
+    public void setRestaurantRef(String restaurantRef) {
+        this.restaurantRef = restaurantRef;
+    }
+
+    public int getFlag() {
+        return flag;
+    }
+
+    public void setFlag(int flag) {
+        this.flag = flag;
+    }
 }
