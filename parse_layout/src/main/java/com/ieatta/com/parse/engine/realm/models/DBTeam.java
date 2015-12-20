3cb04616dc8df7c5e1a30c94e9fca63224258499
Trujun Zhang <trujunzhang@gmail.com>
2015-12-20 21:48:57 +0800

    IEATTA for android.

--- parse_layout/src/main/java/com/ieatta/com/parse/engine/realm/models/DBTeam.java
+++ parse_layout/src/main/java/com/ieatta/com/parse/engine/realm/models/DBTeam.java
@@ -1,5 +1,7 @@
 package com.ieatta.com.parse.engine.realm.models;
 
+import java.util.Date;
+
 import io.realm.RealmObject;
 
 /**
@@ -7,5 +9,51 @@ import io.realm.RealmObject;
  */
 public class DBTeam extends RealmObject {
 
+    public String UUID = "";
+    public Date objectCreatedDate = new Date();
+    public String displayName = "";
 
+
+    public String email = "";
+    public String address = "";
+
+    public String getUUID() {
+        return UUID;
+    }
+
+    public void setUUID(String UUID) {
+        this.UUID = UUID;
+    }
+
+    public Date getObjectCreatedDate() {
+        return objectCreatedDate;
+    }
+
+    public void setObjectCreatedDate(Date objectCreatedDate) {
+        this.objectCreatedDate = objectCreatedDate;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
