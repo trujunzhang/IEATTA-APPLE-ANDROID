3bc4c1359d9b1926491c85039f157f832acd08a2
Trujun Zhang <trujunzhang@gmail.com>
2015-12-20 21:48:00 +0800

    IEATTA for android.

--- parse_layout/src/main/java/com/ieatta/com/parse/engine/realm/models/DBReview.java
+++ parse_layout/src/main/java/com/ieatta/com/parse/engine/realm/models/DBReview.java
@@ -1,11 +1,79 @@
 package com.ieatta.com.parse.engine.realm.models;
 
+import com.ieatta.com.parse.models.enums.ReviewType;
+
+import java.util.Date;
+
 import io.realm.RealmObject;
 
 /**
  * Created by djzhang on 12/20/15.
  */
 public class DBReview extends RealmObject {
+    public String UUID = "";
+    public Date objectCreatedDate = new Date();
 
+    public String content = "";
 
+    /// Rate valuefrom 1 to 5
+    public int rate = 1;
+    public String userRef = "";// Point of Team
+    public String reviewRef = "";
+    public int reviewType = ReviewType.Review_Unknow.ordinal();
+
+    public String getUUID() {
+        return UUID;
+    }
+
+    public void setUUID(String UUID) {
+        this.UUID = UUID;
+    }
+
+    public Date getObjectCreatedDate() {
+        return objectCreatedDate;
+    }
+
+    public void setObjectCreatedDate(Date objectCreatedDate) {
+        this.objectCreatedDate = objectCreatedDate;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getRate() {
+        return rate;
+    }
+
+    public void setRate(int rate) {
+        this.rate = rate;
+    }
+
+    public String getUserRef() {
+        return userRef;
+    }
+
+    public void setUserRef(String userRef) {
+        this.userRef = userRef;
+    }
+
+    public String getReviewRef() {
+        return reviewRef;
+    }
+
+    public void setReviewRef(String reviewRef) {
+        this.reviewRef = reviewRef;
+    }
+
+    public int getReviewType() {
+        return reviewType;
+    }
+
+    public void setReviewType(int reviewType) {
+        this.reviewType = reviewType;
+    }
 }
