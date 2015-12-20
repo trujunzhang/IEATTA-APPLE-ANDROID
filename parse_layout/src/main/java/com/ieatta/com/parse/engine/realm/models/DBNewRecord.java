2f74c9e5954af63a1d8143c22ecfb48e88db800c
Trujun Zhang <trujunzhang@gmail.com>
2015-12-20 21:34:33 +0800

    IEATTA for android.

---
+++ parse_layout/src/main/java/com/ieatta/com/parse/engine/realm/models/DBNewRecord.java
@@ -0,0 +1,11 @@
+package com.ieatta.com.parse.engine.realm.models;
+
+import io.realm.RealmObject;
+
+/**
+ * Created by djzhang on 12/20/15.
+ */
+public class DBNewRecord extends RealmObject {
+
+
+}
