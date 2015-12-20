8fd4d423dd686c6dc56dc3337756b101ed7ad626
Trujun Zhang <trujunzhang@gmail.com>
2015-12-20 23:11:43 +0800

    IEATTA for android.

--- parse_layout/src/main/java/com/ieatta/com/parse/engine/realm/utils/DBAccessUtils.java
+++ parse_layout/src/main/java/com/ieatta/com/parse/engine/realm/utils/DBAccessUtils.java
@@ -2,11 +2,17 @@ package com.ieatta.com.parse.engine.realm.utils;
 
 import android.yelp.com.commonlib.EnvironmentUtils;
 
+import com.ieatta.com.parse.ParseModelAbstract;
 import com.ieatta.com.parse.ParseModelQuery;
-import com.ieatta.com.parse.engine.realm.models.DBTeam;
+import com.ieatta.com.parse.models.Event;
+import com.ieatta.com.parse.models.NewRecord;
+import com.ieatta.com.parse.models.PeopleInEvent;
+import com.ieatta.com.parse.models.Photo;
+import com.ieatta.com.parse.models.Recipe;
+import com.ieatta.com.parse.models.Restaurant;
+import com.ieatta.com.parse.models.Review;
 import com.ieatta.com.parse.models.Team;
 import com.ieatta.com.parse.models.enums.PQueryModelType;
-import com.ieatta.com.parse.models.enums.ReviewType;
 
 import io.realm.Realm;
 
@@ -17,21 +23,53 @@ public class DBAccessUtils {
 
 
     public static void pinInBackground(ParseModelQuery model) {
-        PQueryModelType modelType = model.getModelType();
-        if(modelType.equals(PQueryModelType.Team)){
-            DBAccessUtils.save((Team)model);
-        }
+        DBAccessUtils.save(model);
     }
 
-    private static void save(Team model) {
+    private static void save(ParseModelAbstract model) {
         // Obtain a Realm instance
         Realm realm = Realm.getInstance(EnvironmentUtils.sharedInstance.getGlobalContext());
 
         realm.beginTransaction();
 
         //... add or update objects here ...
-        DBModelConvert.write(realm,model);
+        DBAccessUtils.write(realm, model);
 
         realm.commitTransaction();
     }
+
+    private static void write(Realm realm, ParseModelAbstract model) {
+        PQueryModelType modelType = model.getModelType();
+        switch (modelType) {
+            case Recipe:
+                DBModelConvert.write((Recipe) model, realm);
+                break;
+            case Photo:
+                DBModelConvert.write((Photo) model, realm);
+                break;
+            case Team:
+                DBModelConvert.write((Team) model, realm);
+                break;
+            case Review:
+                DBModelConvert.write((Review) model, realm);
+                break;
+            case Event:
+                DBModelConvert.write((Event) model, realm);
+                break;
+            case Restaurant:
+                DBModelConvert.write((Restaurant) model, realm);
+                break;
+            case NewRecord:
+                DBModelConvert.write((NewRecord) model, realm);
+                break;
+            case PeopleInEvent:
+                DBModelConvert.write((PeopleInEvent) model, realm);
+                break;
+            default:
+                break;
+        }
+
+    }
+
+
 }
--- parse_layout/src/main/java/com/ieatta/com/parse/engine/realm/utils/DBModelConvert.java
+++ parse_layout/src/main/java/com/ieatta/com/parse/engine/realm/utils/DBModelConvert.java
@@ -2,6 +2,13 @@ package com.ieatta.com.parse.engine.realm.utils;
 
 import com.ieatta.com.parse.ParseModelAbstract;
 import com.ieatta.com.parse.engine.realm.models.DBTeam;
+import com.ieatta.com.parse.models.Event;
+import com.ieatta.com.parse.models.NewRecord;
+import com.ieatta.com.parse.models.PeopleInEvent;
+import com.ieatta.com.parse.models.Photo;
+import com.ieatta.com.parse.models.Recipe;
+import com.ieatta.com.parse.models.Restaurant;
+import com.ieatta.com.parse.models.Review;
 import com.ieatta.com.parse.models.Team;
 
 import io.realm.Realm;
@@ -11,7 +18,34 @@ import io.realm.Realm;
  */
 public class DBModelConvert {
 
-    public static void write( Realm realm, Team model) {
+    public static void write(Event model, Realm realm) {
+
+    }
+
+    public static void write(NewRecord model, Realm realm) {
+
+    }
+
+    public static void write(PeopleInEvent model, Realm realm) {
+
+    }
+
+    public static void write(Photo model, Realm realm) {
+
+    }
+
+    public static void write(Recipe model, Realm realm) {
+
+    }
+
+    public static void write(Restaurant model, Realm realm) {
+
+    }
+    public static void write(Review model, Realm realm) {
+
+    }
+
+    public static void write(Team model, Realm realm) {
         DBTeam team = realm.createObject(DBTeam.class); // Create a new object
         team.setUUID(ParseModelAbstract.getPoint(model));
         team.setObjectCreatedDate(model.objectCreatedDate);
