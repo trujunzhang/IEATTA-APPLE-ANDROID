f3436604b5b960a6602e3487a32c4b11e0c0b276
Trujun Zhang <trujunzhang@gmail.com>
2015-12-21 09:47:13 +0800

    IEATTA for android.

--- parse_layout/src/main/java/com/ieatta/com/parse/engine/realm/utils/RMBuilder.java
+++ parse_layout/src/main/java/com/ieatta/com/parse/engine/realm/utils/RMBuilder.java
@@ -13,21 +13,19 @@ public class RMBuilder<E extends RealmObject> {
 
     // boolean, short, ìnt, long, float, double, String, Date
     public RealmQuery<E> buildEqualTo(RealmQuery<E> where, HashMap<String, Object> equalMap) {
-        equalMap.keySet();
 
         for(String key : equalMap.keySet()){
             Object value = equalMap.get(key);
 
             if(value.getClass().equals(String.class)){
-                where.equalTo(key, (String) value);
+                where = where.equalTo(key, (String) value);
             }else if(value.getClass().equals(Integer.class)){
-                where.equalTo(key, (int) value);
+                where = where.equalTo(key, (int) value);
             }else{
                 Class<?> aClass = value.getClass();
                 int x = 0;
             }
         }
-
 
         return where;
     }
