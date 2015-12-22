e98a58c980663aef1921c9b294eba85a7c073bd7
Trujun Zhang <trujunzhang@gmail.com>
2015-12-22 17:02:28 +0800

    IEATTA for android.

--- parse_layout/src/main/java/com/ieatta/com/parse/engine/realm/utils/RMQuery.java
+++ parse_layout/src/main/java/com/ieatta/com/parse/engine/realm/utils/RMQuery.java
@@ -6,26 +6,27 @@ import com.ieatta.com.parse.engine.realm.DBBuilder;
 import java.util.List;
 
 import bolts.Task;
+import io.realm.RealmObject;
 import io.realm.RealmResults;
 
 /**
  * Created by djzhang on 12/21/15.
  */
-public class RMQuery<T extends ParseModelAbstract> {
+public class RMQuery<T extends RealmObject> {
     private DBBuilder builder;
 
     public RMQuery(DBBuilder builder) {
         this.builder = builder;
     }
 
-    public Task<List<T>> findInBackground() {
+    public Task<List<ParseModelAbstract>> findInBackground() {
 
 //        RealmResults results =  builder.where.equalTo("restaurantRef", "1CE562A4-A978-4B75-9B7B-2F3CF9F42A04").findAll();
         RealmResults results = builder.buildAll().findAll();
 
         results = builder.sort(results);
 
-        List<T> list = new DBModelReader().readRealmResults(results, builder.modelType, builder.limit);
+        List<ParseModelAbstract> list = new DBModelReader().readRealmResults(results, builder.modelType, builder.limit);
 
         return Task.forResult(list);
     }
