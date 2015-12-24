734c6326c52a1926133eec075e44a3547c553b8f
Trujun Zhang <trujunzhang@gmail.com>
2015-12-24 22:30:53 +0800

    IEATTA for android.

--- app/src/main/java/com/ieatta/android/modules/cells/IEAOrderedPeopleCell.java
+++ app/src/main/java/com/ieatta/android/modules/cells/IEAOrderedPeopleCell.java
@@ -2,12 +2,17 @@ package com.ieatta.android.modules.cells;
 
 import android.view.View;
 import android.widget.TextView;
+import android.yelp.com.commonlib.EnvironmentUtils;
 
 import com.ieatta.android.R;
 import com.ieatta.android.extensions.storage.models.CellType;
 import com.ieatta.android.extensions.viewkit.AvatarView;
 import com.ieatta.android.modules.adapter.IEAViewHolder;
 import com.ieatta.android.modules.cells.model.IEAOrderedPeople;
+import com.ieatta.com.parse.models.Recipe;
+
+import bolts.Continuation;
+import bolts.Task;
 
 public class IEAOrderedPeopleCell extends IEAViewHolder {
     public static CellType getType() {
@@ -30,26 +35,36 @@ public class IEAOrderedPeopleCell extends IEAViewHolder {
 
     @Override
     public void render(Object value) {
-        IEAOrderedPeople model = (IEAOrderedPeople) value;
+        final IEAOrderedPeople model = (IEAOrderedPeople) value;
         self.nameLabel.setText(model.model.displayName);
         self.addressLabel.setText(model.model.address);
         self.avatarView.loadNewPhotoByModel(model.model, R.drawable.blank_user_small);
 
         // TODO: djzhang: fixing
-//        Recipe.queryOrderedRecipesCount(model.model!, event: model.event!).continueWithBlock { (task) -> AnyObject? in
-//
-//            dispatch_async(dispatch_get_main_queue(), { () -> Void in
-//
-//            if let _ = task.error{
-//                self.setRecipeInformation(L10n.FetchRecipeCountFailure.string)
-//            }else{
-//                let recipesCount = task.result as! Int
-//                model.model?.recipesCount = recipesCount
-//                self.setRecipeInformation(tr(L10n.ReipesOrderedCount(recipesCount)))
-//            }
-//            })
-//
-//            return nil
-//        }
+        Recipe.queryOrderedRecipesCount(model.model, model.event).onSuccess(new Continuation<Integer, Object>() {
+            @Override
+            public Object then(Task<Integer> task) throws Exception {
+
+                int recipesCount = task.getResult();
+                model.model.recipesCount = recipesCount;
+                String info = EnvironmentUtils.sharedInstance.getGlobalContext().getResources().getString(R.string.Recipes_Ordered_Count);
+                info = info + ": " + recipesCount;
+                self.setRecipeInformation(info);
+                return null;
+            }
+        }).continueWith(new Continuation<Object, Object>() {
+            @Override
+            public Object then(Task<Object> task) throws Exception {
+                if (task.isFaulted()) {
+                    String info = EnvironmentUtils.sharedInstance.getGlobalContext().getResources().getString(R.string.Fetch_recipe_count_failure);
+                    self.setRecipeInformation(info);
+                }
+                return null;
+            }
+        });
+    }
+
+    private void setRecipeInformation(String info){
+        self.addressLabel.setText(info);
     }
 }
