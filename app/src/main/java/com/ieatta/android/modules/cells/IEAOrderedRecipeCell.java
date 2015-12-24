233810943f6e76ce78c150ca5eeffc4eade85ae3
Trujun Zhang <trujunzhang@gmail.com>
2015-12-24 22:11:18 +0800

    IEATTA for android.

--- app/src/main/java/com/ieatta/android/modules/cells/IEAOrderedRecipeCell.java
+++ app/src/main/java/com/ieatta/android/modules/cells/IEAOrderedRecipeCell.java
@@ -7,6 +7,7 @@ import android.widget.TextView;
 import com.ieatta.android.R;
 import com.ieatta.android.extensions.storage.models.CellType;
 import com.ieatta.android.extensions.viewkit.AvatarView;
+import com.ieatta.android.extensions.viewkit.RatingImageView;
 import com.ieatta.android.modules.adapter.IEAViewHolder;
 import com.ieatta.com.parse.models.Recipe;
 
@@ -21,14 +22,14 @@ public class IEAOrderedRecipeCell extends IEAViewHolder {
 
     private TextView displayNameLabel;
     private TextView priceLabel;
-    private ImageView ratingImageView;
+    private RatingImageView ratingImageView;
 
     public IEAOrderedRecipeCell(View itemView) {
         super(itemView);
         self.avatarView = (AvatarView) itemView.findViewById(R.id.avatarView);
         self.displayNameLabel = (TextView) itemView.findViewById(R.id.titleTextView);
         self.priceLabel = (TextView) itemView.findViewById(R.id.priceTextView);
-        self.ratingImageView = (ImageView) itemView.findViewById(R.id.business_review_star_rating);
+        self.ratingImageView = (RatingImageView) itemView.findViewById(R.id.business_review_star_rating);
     }
 
     @Override
@@ -36,7 +37,9 @@ public class IEAOrderedRecipeCell extends IEAViewHolder {
         Recipe model  = (Recipe) value;
         self.displayNameLabel.setText(model.displayName);
         self.priceLabel.setText(model.cost+"");
-//        self.ratingImageView.setImageLevel(model.);
+
+        self.ratingImageView.queryRatingInReviewsByModel(model);
+
         self.avatarView.loadNewPhotoByModel(model, R.drawable.placeholder_photo);
     }
 }
