17aa3e746a0e8fd4e7333204e521972d0355409d
Trujun Zhang <trujunzhang@gmail.com>
2015-12-25 08:49:18 +0800

    IEATTA for android.

--- app/src/main/java/com/ieatta/android/modules/view/posts/IEAWriteReviewViewController.java
+++ app/src/main/java/com/ieatta/android/modules/view/posts/IEAWriteReviewViewController.java
@@ -121,23 +121,6 @@ public class IEAWriteReviewViewController extends IEAAppSegureTableViewControlle
         ratingImageView.setBackgroundResource(self.getRatingTitleImage(tag));
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_post_review) {
-//
-//            this.saveModel();
-//            return true;
-//        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     // MARK: Navigation item actions
     private void postAction() {
 
