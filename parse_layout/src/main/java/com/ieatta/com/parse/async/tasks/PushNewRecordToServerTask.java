1161e93ccb56fbac9fb01acd1368349504d84641
Trujun Zhang <trujunzhang@gmail.com>
2015-12-25 23:10:49 +0800

    IEATTA for android.

--- parse_layout/src/main/java/com/ieatta/com/parse/async/tasks/PushNewRecordToServerTask.java
+++ parse_layout/src/main/java/com/ieatta/com/parse/async/tasks/PushNewRecordToServerTask.java
@@ -4,6 +4,7 @@ import android.yelp.com.commonlib.LogUtils;
 
 import com.ieatta.com.parse.ParseModelAbstract;
 
+import com.ieatta.com.parse.ParseModelLocalQuery;
 import com.ieatta.com.parse.async.SerialTasksManager;
 import com.ieatta.com.parse.engine.realm.LocalQuery;
 import com.ieatta.com.parse.models.NewRecord;
@@ -22,22 +23,19 @@ public class PushNewRecordToServerTask {
 
     public static Task PushToServerSeriesTask(LocalQuery query) {
 
-        // TODO: djzhang(fixing)
-//        return ParseMoxdelQuery.findInBackgroundFromRealm(query).onSuccessTask(new Continuation<List<ParseObject>, Task<Void>>() {
-//            @Override
-//            public Task<Void> then(Task<List<ParseObject>> task) throws Exception {
-//                return executeSerialTasks(task);
-//            }
-//        });
-
-        return Task.forResult(null);
+        return ParseModelLocalQuery.findInBackgroundFromRealm(query).onSuccessTask(new Continuation<List<ParseModelAbstract>, Task>() {
+            @Override
+            public Task then(Task<List<ParseModelAbstract>> task) throws Exception {
+                return executeSerialTasks(task);
+            }
+        });
     }
 
-    private static Task executeSerialTasks(Task previous) {
-        List<ParseObject> results = (List<ParseObject>) previous.getResult();
+    private static Task executeSerialTasks(Task<List<ParseModelAbstract>> previous) {
+        List<ParseModelAbstract> results = previous.getResult();
         LogUtils.debug("{ count in Pull objects from Server }: " + results.size());
 
-        SerialTasksManager manager = new SerialTasksManager(results);
+        SerialTasksManager<ParseModelAbstract> manager = new SerialTasksManager<>(results);
         if (manager.hasNext() == false) {
             return Task.forResult(true);
         }
@@ -45,8 +43,9 @@ public class PushNewRecordToServerTask {
         return startPullFromServerSingleTask(manager);
     }
 
-    private static Task startPullFromServerSingleTask(final SerialTasksManager manager) {
-        return PushObjectToServerTask(manager.next()).onSuccessTask(new Continuation() {
+    private static Task startPullFromServerSingleTask(final SerialTasksManager<ParseModelAbstract> manager) {
+        return PushObjectToServerTask((NewRecord) manager.next())
+                .onSuccessTask(new Continuation() {
             @Override
             public Object then(Task task) throws Exception {
                 if (manager.hasNext() == false) {
@@ -62,16 +61,15 @@ public class PushNewRecordToServerTask {
      * <p/>
      * - parameter newRecordObject: A row data on the NewRecord table.
      */
-    private static Task PushObjectToServerTask(final ParseObject newRecordObject) {
-        // Convert newRecordObject to Model instance.
-        final NewRecord newRecord = (NewRecord) new NewRecord().convertToLocalModel(newRecordObject);
-
+    private static Task PushObjectToServerTask(final NewRecord newRecord) {
 //        LogUtils.debug(" [ newRecord in push to server ]: " + newRecord.printDescription());
 
         // 1. Get the recoreded emptyModel instance from NewRecord, by the modelType and the modelPoint.
         // (such as photo,restaurant,event etc)
         final ParseModelAbstract emptyModel = newRecord.getRecordedModel();
 //        LogUtils.debug(" [ emptyModel in push to server ]: " + emptyModel.printDescription());
+
+
 
         return emptyModel.pushToServer().onSuccessTask(new Continuation<Object, Task<Void>>() {
             @Override
