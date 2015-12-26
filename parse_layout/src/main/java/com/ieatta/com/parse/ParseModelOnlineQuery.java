5499b76d8d37e0bc57555b16d1c0ded412b7969a
Trujun Zhang <trujunzhang@gmail.com>
2015-12-26 23:25:25 +0800

    IEATTA for android.

--- parse_layout/src/main/java/com/ieatta/com/parse/ParseModelOnlineQuery.java
+++ parse_layout/src/main/java/com/ieatta/com/parse/ParseModelOnlineQuery.java
@@ -158,7 +158,11 @@ public abstract class ParseModelOnlineQuery extends ParseModelConvert {
     @Override
     public Task<Void> saveParseObjectToServer() {
         ParseObject object = makeObject();
-        self.writeLocalObject(object);
+        // **** Important ****
+        // Before saving to Parse.com.
+        // We must convert the localdata model to ParseObject.
+        // Using 'writeObject'.
+        self.writeObject(object);
 
         return object.saveInBackground();
     }
