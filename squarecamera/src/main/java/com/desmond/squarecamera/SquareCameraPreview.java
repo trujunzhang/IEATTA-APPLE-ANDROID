8bbb8d745edaed3897e4ef88c341f02f5028f38b
Trujun Zhang <trujunzhang@gmail.com>
2015-12-27 19:59:12 +0800

    IEATTA for android.

--- squarecamera/src/main/java/com/desmond/squarecamera/SquareCameraPreview.java
+++ squarecamera/src/main/java/com/desmond/squarecamera/SquareCameraPreview.java
@@ -79,22 +79,22 @@ public class SquareCameraPreview extends SurfaceView {
         int height = MeasureSpec.getSize(heightMeasureSpec);
         int width = MeasureSpec.getSize(widthMeasureSpec);
 
-        final boolean isPortrait =
-                getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT;
-
-        if (isPortrait) {
-            if (width > height * ASPECT_RATIO) {
-                width = (int) (height * ASPECT_RATIO + 0.5);
-            } else {
-                height = (int) (width / ASPECT_RATIO + 0.5);
-            }
-        } else {
-            if (height > width * ASPECT_RATIO) {
-                height = (int) (width * ASPECT_RATIO + 0.5);
-            } else {
-                width = (int) (height / ASPECT_RATIO + 0.5);
-            }
-        }
+//        final boolean isPortrait =
+//                getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT;
+//
+//        if (isPortrait) {
+//            if (width > height * ASPECT_RATIO) {
+//                width = (int) (height * ASPECT_RATIO + 0.5);
+//            } else {
+//                height = (int) (width / ASPECT_RATIO + 0.5);
+//            }
+//        } else {
+//            if (height > width * ASPECT_RATIO) {
+//                height = (int) (width * ASPECT_RATIO + 0.5);
+//            } else {
+//                width = (int) (height / ASPECT_RATIO + 0.5);
+//            }
+//        }
 
         setMeasuredDimension(width, height);
     }
