86f52d4e3a2b09746a09148e4d6df03d0aea15be
Trujun Zhang <trujunzhang@gmail.com>
2016-01-02 17:08:41 +0800

    IEATTA for Android.

---
+++ commonLib/src/main/java/com/ieatta/android/notification/NSNotificationCenter.java
@@ -0,0 +1,20 @@
+package com.ieatta.android.notification;
+
+import de.greenrobot.event.EventBus;
+
+/**
+ * Created by djzhang on 12/15/15.
+ */
+public class NSNotificationCenter {
+    private static NSNotificationCenter instance = new NSNotificationCenter();
+
+    public static NSNotificationCenter defaultCenter() {
+        return instance;
+    }
+
+    public void postNotificationName(NotifyType type, Object anObject) {
+        EventBus.getDefault().post(
+                new NSNotification(type, anObject));
+    }
+
+}
