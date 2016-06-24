6a1b0cf569cae04e103b34fde94d797bc02df113
trujunzhang <trujunzhang@gmail.com>
2016-06-24 20:54:07 +0800

    ieatta.

---
+++ IEATTA/src/androidTest/java/org/ieatta/database/realm/RealmModelReaderTest.java
@@ -0,0 +1,16 @@
+package org.ieatta.database.realm;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class RealmModelReaderTest {
+
+    @Test
+    public void testFetchRatingReview() throws Exception {
+
+    }
+}
