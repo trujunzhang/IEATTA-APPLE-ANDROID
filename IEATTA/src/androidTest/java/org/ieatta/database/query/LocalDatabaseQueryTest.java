10d156da6e7b665f584ca9738f5f5ce10938c7ca
trujunzhang <trujunzhang@gmail.com>
2016-06-24 20:55:55 +0800

    ieatta.

--- IEATTA/src/androidTest/java/org/ieatta/database/query/LocalDatabaseQueryTest.java
+++ IEATTA/src/androidTest/java/org/ieatta/database/query/LocalDatabaseQueryTest.java
@@ -1,4 +1,4 @@
-package org.ieatta.database.realm;
+package org.ieatta.database.query;
 
 import android.support.test.runner.AndroidJUnit4;
 
@@ -7,7 +7,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(AndroidJUnit4.class)
-public class RealmModelReaderTest {
+public class LocalDatabaseQueryTest {
     @Before
     public void setUp() throws Exception {
 
